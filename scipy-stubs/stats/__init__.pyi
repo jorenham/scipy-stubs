e88f80e81b42ef12cc8ddd583e17f00317160526
jorenham <jhammudoglu@gmail.com>
2024-08-27 15:45:17 +0200

    cleanup imports in `scipy.stats`

--- scipy-stubs/stats/__init__.pyi
+++ scipy-stubs/stats/__init__.pyi
@@ -1,50 +1,34 @@
-from . import (
-    biasedurn as biasedurn,
-    contingency as contingency,
-    kde as kde,
-    morestats as morestats,
-    mstats as mstats,
-    mstats_basic as mstats_basic,
-    mstats_extras as mstats_extras,
-    mvn as mvn,
-    qmc as qmc,
-    stats as stats,
-)
+from . import contingency, mstats, qmc
 from ._binned_statistic import *
-from ._binomtest import binomtest as binomtest
-from ._bws_test import bws_test as bws_test
-from ._censored_data import CensoredData as CensoredData
-from ._covariance import Covariance as Covariance
+from ._binomtest import binomtest
+from ._bws_test import bws_test
+from ._censored_data import CensoredData
+from ._covariance import Covariance
 from ._entropy import *
-from ._fit import fit as fit, goodness_of_fit as goodness_of_fit
+from ._fit import fit, goodness_of_fit
 from ._hypotests import *
-from ._kde import gaussian_kde as gaussian_kde
-from ._mannwhitneyu import mannwhitneyu as mannwhitneyu
-from ._mgc import multiscale_graphcorr as multiscale_graphcorr
+from ._kde import gaussian_kde
+from ._mannwhitneyu import mannwhitneyu
+from ._mgc import multiscale_graphcorr
 from ._morestats import *
 from ._multicomp import *
 from ._multivariate import *
-from ._page_trend_test import page_trend_test as page_trend_test
+from ._page_trend_test import page_trend_test
 from ._resampling import (
-    BootstrapMethod as BootstrapMethod,
-    MonteCarloMethod as MonteCarloMethod,
-    PermutationMethod as PermutationMethod,
-    bootstrap as bootstrap,
-    monte_carlo_test as monte_carlo_test,
-    permutation_test as permutation_test,
-    power as power,
+    BootstrapMethod,
+    MonteCarloMethod,
+    PermutationMethod,
+    bootstrap,
+    monte_carlo_test,
+    permutation_test,
+    power,
 )
 from ._sensitivity_analysis import *
 from ._stats_py import *
 from ._survival import *
-from ._variation import variation as variation
-from ._warnings_errors import (
-    ConstantInputWarning as ConstantInputWarning,
-    DegenerateDataWarning as DegenerateDataWarning,
-    FitError as FitError,
-    NearConstantInputWarning as NearConstantInputWarning,
-)
-from .contingency import chi2_contingency as chi2_contingency
+from ._variation import variation
+from ._warnings_errors import ConstantInputWarning, DegenerateDataWarning, FitError, NearConstantInputWarning
+from .contingency import chi2_contingency
 from .distributions import *
 
 __all__ = [
