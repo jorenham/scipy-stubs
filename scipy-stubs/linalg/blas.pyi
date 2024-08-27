463ba37a331d05b6231118845496bb66ddd8a471
jorenham <jhammudoglu@gmail.com>
2024-08-27 11:24:05 +0200

    type-hint `scipy.linalg.blas`

--- scipy-stubs/linalg/blas.pyi
+++ scipy-stubs/linalg/blas.pyi
@@ -1,8 +1,40 @@
-from scipy._typing import Untyped
-from scipy.linalg._fblas import *
+from collections.abc import Iterable, Sequence
+from typing import Any, Literal, Protocol, type_check_only
 
-HAS_ILP64: bool
-empty_module: Untyped
+import numpy as np
+import numpy.typing as npt
+from typing_extensions import LiteralString
 
-def find_best_blas_type(arrays=(), dtype: Untyped | None = None) -> Untyped: ...
-def get_blas_funcs(names, arrays=(), dtype: Untyped | None = None, ilp64: bool = False) -> Untyped: ...
+__all__ = ["find_best_blas_type", "get_blas_funcs"]
+
+@type_check_only
+class _FortranFunction(Protocol):
+    @property
+    def dtype(self) -> np.dtype[np.number[Any]]: ...  # type: ignore[no-any-explicit]
+    @property
+    def int_dtype(self) -> np.dtype[np.integer[Any]]: ...  # type: ignore[no-any-explicit]
+    @property
+    def module_name(self) -> LiteralString: ...
+    @property
+    def prefix(self) -> LiteralString: ...
+    @property
+    def typecode(self) -> LiteralString: ...
+
+    def __call__(self, /, *args: object, **kwargs: object) -> object: ...
+
+def find_best_blas_type(
+    arrays: Sequence[npt.NDArray[np.generic]] = (),
+    dtype: npt.DTypeLike | None = None,
+) -> (
+    # see `scipy.linalg.blas._type_conv`
+    tuple[Literal["s"], np.dtypes.Float32DType, bool]
+    | tuple[Literal["f"], np.dtypes.Float64DType, bool]
+    | tuple[Literal["c"], np.dtypes.Complex64DType, bool]
+    | tuple[Literal["z"], np.dtypes.Complex128DType, bool]
+): ...
+def get_blas_funcs(
+    names: Iterable[str] | str,
+    arrays: Sequence[npt.NDArray[np.generic]] = (),
+    dtype: npt.DTypeLike | None = None,
+    ilp64: Literal[True, False, "preferred"] = False,
+) -> list[_FortranFunction] | _FortranFunction: ...
