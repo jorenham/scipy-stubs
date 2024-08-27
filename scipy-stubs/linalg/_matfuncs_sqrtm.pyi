9aa938b7619fca4f12de6ccedf8d70081eaad9aa
jorenham <jhammudoglu@gmail.com>
2024-08-27 10:21:12 +0200

    type-hint `scipy.linalg._matfuncs*`

--- scipy-stubs/linalg/_matfuncs_sqrtm.pyi
+++ scipy-stubs/linalg/_matfuncs_sqrtm.pyi
@@ -1,11 +1,15 @@
-import numpy as np
+from typing import Literal, TypeAlias, overload
 
-from ._decomp_schur import rsf2csf as rsf2csf, schur as schur
-from ._matfuncs_sqrtm_triu import within_block_loop as within_block_loop
-from ._misc import norm as norm
-from .lapack import dtrsyl as dtrsyl, ztrsyl as ztrsyl
-from scipy._typing import Untyped
+import numpy as np
+import numpy.typing as npt
+
+__all__ = ["sqrtm"]
+
+_Array_fc_2d: TypeAlias = np.ndarray[tuple[int, int], np.dtype[np.inexact[npt.NBitBase]]]
 
 class SqrtmError(np.linalg.LinAlgError): ...
 
-def sqrtm(A, disp: bool = True, blocksize: int = 64) -> Untyped: ...
+@overload
+def sqrtm(A: npt.ArrayLike, disp: Literal[True] = True, blocksize: int = 64) -> _Array_fc_2d: ...
+@overload
+def sqrtm(A: npt.ArrayLike, disp: Literal[False], blocksize: int = 64) -> tuple[_Array_fc_2d, float | np.float64]: ...
