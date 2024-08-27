d02b82e0688e9380dd67da718158d1c06f1fd8a8
jorenham <jhammudoglu@gmail.com>
2024-08-27 10:31:21 +0200

    type-hint `scipy.linalg._sketches`

--- scipy-stubs/_typing.pyi
+++ scipy-stubs/_typing.pyi
@@ -5,7 +5,7 @@ import numpy as np
 import numpy.typing as npt
 from typing_extensions import TypeVar
 
-__all__ = "AnyBool", "AnyChar", "AnyComplex", "AnyInt", "AnyReal", "AnyScalar", "Array0D", "Untyped"
+__all__ = "AnyBool", "AnyChar", "AnyComplex", "AnyInt", "AnyReal", "AnyScalar", "Array0D", "Seed", "Untyped"
 
 # placeholder for missing annotations
 Untyped: TypeAlias = object
@@ -20,3 +20,6 @@ AnyReal: TypeAlias = int | float | np.floating[npt.NBitBase] | np.integer[npt.NB
 AnyComplex: TypeAlias = int | float | complex | np.number[npt.NBitBase] | np.bool_
 AnyChar: TypeAlias = str | bytes  # `np.str_ <: builtins.str` and `np.bytes_ <: builtins.bytes`
 AnyScalar: TypeAlias = int | float | complex | AnyChar | np.generic
+
+# numpy.random
+Seed: TypeAlias = int | np.random.Generator | np.random.RandomState
