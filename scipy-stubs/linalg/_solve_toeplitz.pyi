89830e8428fcb8e95419d9dc04348b6606d510ca
jorenham <jhammudoglu@gmail.com>
2024-08-27 10:40:54 +0200

    type-hint `scipy.linalg._solvers`

---
+++ scipy-stubs/linalg/_solve_toeplitz.pyi
@@ -0,0 +1,10 @@
+from typing import TypeAlias
+
+import numpy as np
+import numpy.typing as npt
+
+__all__ = ("levinson",)
+
+_Array_fc_1d: TypeAlias = np.ndarray[tuple[int], np.dtype[np.float64 | np.complex128]]
+
+def levinson(a: npt.NDArray[np.float64 | np.complex128], b: npt.NDArray[np.float64 | np.complex128]) -> tuple[_Array_fc_1d]: ...
--- scipy-stubs/linalg/_solvers.pyi
+++ scipy-stubs/linalg/_solvers.pyi
@@ -1,17 +1,44 @@
-from ._basic import matrix_balance as matrix_balance, solve as solve, solve_triangular as solve_triangular
-from ._decomp_lu import lu as lu
-from ._decomp_qr import qr as qr
-from ._decomp_qz import ordqz as ordqz
-from ._decomp_schur import schur as schur
-from ._special_matrices import block_diag as block_diag, kron as kron
-from .lapack import get_lapack_funcs as get_lapack_funcs
-from scipy._typing import Untyped
+from typing import Literal, TypeAlias
 
-def solve_sylvester(a, b, q) -> Untyped: ...
-def solve_continuous_lyapunov(a, q) -> Untyped: ...
+import numpy as np
+import numpy.typing as npt
+
+__all__ = [
+    "solve_continuous_are",
+    "solve_continuous_lyapunov",
+    "solve_discrete_are",
+    "solve_discrete_lyapunov",
+    "solve_lyapunov",
+    "solve_sylvester",
+]
+
+_Array_fc_2d: TypeAlias = np.ndarray[tuple[int, int], np.dtype[np.inexact[npt.NBitBase]]]
+
+def solve_sylvester(a: npt.ArrayLike, b: npt.ArrayLike, q: npt.ArrayLike) -> _Array_fc_2d: ...
+def solve_continuous_lyapunov(a: npt.ArrayLike, q: npt.ArrayLike) -> _Array_fc_2d: ...
 
 solve_lyapunov = solve_continuous_lyapunov
 
-def solve_discrete_lyapunov(a, q, method: Untyped | None = None) -> Untyped: ...
-def solve_continuous_are(a, b, q, r, e: Untyped | None = None, s: Untyped | None = None, balanced: bool = True) -> Untyped: ...
-def solve_discrete_are(a, b, q, r, e: Untyped | None = None, s: Untyped | None = None, balanced: bool = True) -> Untyped: ...
+def solve_discrete_lyapunov(
+    a: npt.ArrayLike,
+    q: npt.ArrayLike,
+    method: Literal["direct", "bilinear"] | None = None,
+) -> _Array_fc_2d: ...
+def solve_continuous_are(
+    a: npt.ArrayLike,
+    b: npt.ArrayLike,
+    q: npt.ArrayLike,
+    r: npt.ArrayLike,
+    e: npt.ArrayLike | None = None,
+    s: npt.ArrayLike | None = None,
+    balanced: bool = True,
+) -> _Array_fc_2d: ...
+def solve_discrete_are(
+    a: npt.ArrayLike,
+    b: npt.ArrayLike,
+    q: npt.ArrayLike,
+    r: npt.ArrayLike,
+    e: npt.ArrayLike | None = None,
+    s: npt.ArrayLike | None = None,
+    balanced: bool = True,
+) -> _Array_fc_2d: ...
