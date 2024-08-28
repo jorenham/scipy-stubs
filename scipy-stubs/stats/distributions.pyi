a181782a5538e2bc81a0c8283980e88679a75710
jorenham <jhammudoglu@gmail.com>
2024-08-28 20:54:52 +0200

    type-hint `scipy.stats.distributions`

--- scipy-stubs/stats/distributions.pyi
+++ scipy-stubs/stats/distributions.pyi
@@ -1,5 +1,10 @@
+from . import _continuous_distns, _discrete_distns
 from ._continuous_distns import *
 from ._discrete_distns import *
-from ._distn_infrastructure import rv_continuous as rv_continuous, rv_discrete as rv_discrete, rv_frozen as rv_frozen
-from ._entropy import entropy as entropy
-from ._levy_stable import levy_stable as levy_stable
+from ._distn_infrastructure import rv_continuous, rv_discrete, rv_frozen
+from ._entropy import entropy
+from ._levy_stable import levy_stable
+
+__all__ = ["entropy", "levy_stable", "rv_continuous", "rv_discrete", "rv_frozen"]
+__all__ += _continuous_distns.__all__
+__all__ += _discrete_distns.__all__
