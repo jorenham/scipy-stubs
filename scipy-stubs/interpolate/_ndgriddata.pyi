f0c86c5c1425bc029606f68fdcef3847db530a9d
jorenham <jhammudoglu@gmail.com>
2024-08-29 19:50:20 +0200

    fix import errors in `scipy.interpolate`

--- scipy-stubs/interpolate/_ndgriddata.pyi
+++ scipy-stubs/interpolate/_ndgriddata.pyi
@@ -1,16 +1,36 @@
+from typing import Any
+from typing_extensions import override
+
+import numpy as np
+
+import numpy.typing as npt
+import optype.numpy as onpt
 from scipy._typing import Untyped
 
-from scipy.spatial import cKDTree as cKDTree
-from .interpnd import (
-    CloughTocher2DInterpolator as CloughTocher2DInterpolator,
-    LinearNDInterpolator as LinearNDInterpolator,
-    NDInterpolatorBase as NDInterpolatorBase,
-)
+from .interpnd import CloughTocher2DInterpolator, LinearNDInterpolator, NDInterpolatorBase
+
+__all__ = ["CloughTocher2DInterpolator", "LinearNDInterpolator", "NearestNDInterpolator", "griddata"]
 
 class NearestNDInterpolator(NDInterpolatorBase):
     tree: Untyped
     values: Untyped
-    def __init__(self, x, y, rescale: bool = False, tree_options: Untyped | None = None): ...
-    def __call__(self, *args, **query_options) -> Untyped: ...
+    @override
+    def __init__(
+        self,
+        /,
+        x: onpt.AnyIntegerArray | onpt.AnyFloatingArray,
+        y: onpt.AnyFloatingArray,
+        rescale: bool = False,
+        tree_options: Untyped | None = None,
+    ) -> None: ...
+    @override
+    def __call__(self, /, *args: onpt.AnyFloatingArray, **query_options: Untyped) -> npt.NDArray[np.floating[Any]]: ...
 
-def griddata(points, values, xi, method: str = "linear", fill_value=..., rescale: bool = False) -> Untyped: ...
+def griddata(
+    points: onpt.AnyIntegerArray | onpt.AnyFloatingArray,
+    values: onpt.AnyFloatingArray,
+    xi: Untyped,
+    method: str = "linear",
+    fill_value: float = ...,
+    rescale: bool = False,
+) -> Untyped: ...
--- scipy-stubs/interpolate/_rgi.pyi
+++ scipy-stubs/interpolate/_rgi.pyi
@@ -1,10 +1,6 @@
 from scipy._typing import Untyped
 
-from ._bsplines import make_interp_spline as make_interp_spline
-from ._cubic import PchipInterpolator as PchipInterpolator
-from ._fitpack2 import RectBivariateSpline as RectBivariateSpline
-from ._ndbspline import make_ndbspl as make_ndbspl
-from ._rgi_cython import evaluate_linear_2d as evaluate_linear_2d, find_indices as find_indices
+__all__ = ["RegularGridInterpolator", "interpn"]
 
 class RegularGridInterpolator:
     method: Untyped
@@ -13,15 +9,22 @@ class RegularGridInterpolator:
     fill_value: Untyped
     def __init__(
         self,
-        points,
-        values,
+        points: Untyped,
+        values: Untyped,
         method: str = "linear",
         bounds_error: bool = True,
-        fill_value=...,
+        fill_value: Untyped = ...,
         *,
         solver: Untyped | None = None,
         solver_args: Untyped | None = None,
-    ): ...
-    def __call__(self, xi, method: Untyped | None = None, *, nu: Untyped | None = None) -> Untyped: ...
+    ) -> None: ...
+    def __call__(self, xi: Untyped, method: Untyped | None = None, *, nu: Untyped | None = None) -> Untyped: ...
 
-def interpn(points, values, xi, method: str = "linear", bounds_error: bool = True, fill_value=...) -> Untyped: ...
+def interpn(
+    points: Untyped,
+    values: Untyped,
+    xi: Untyped,
+    method: str = "linear",
+    bounds_error: bool = True,
+    fill_value: Untyped = ...,
+) -> Untyped: ...
