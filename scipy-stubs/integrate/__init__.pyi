1e950513bbfc8d548c15bec1ac56bd2a0ddadc43
jorenham <jhammudoglu@gmail.com>
2024-09-03 23:38:32 +0200

    explicit `scipy.integrate.__all__`

--- scipy-stubs/integrate/__init__.pyi
+++ scipy-stubs/integrate/__init__.pyi
@@ -1,20 +1,40 @@
-from . import dop as dop, lsoda as lsoda, odepack as odepack, quadpack as quadpack, vode as vode
-from ._bvp import solve_bvp as solve_bvp
+from . import _ode, _odepack_py, _quadpack_py, _quadrature, dop, lsoda, odepack, quadpack, vode
+from ._bvp import solve_bvp
 from ._ivp import (
-    BDF as BDF,
-    DOP853 as DOP853,
-    LSODA as LSODA,
-    RK23 as RK23,
-    RK45 as RK45,
-    DenseOutput as DenseOutput,
-    OdeSolution as OdeSolution,
-    OdeSolver as OdeSolver,
-    Radau as Radau,
-    solve_ivp as solve_ivp,
+    BDF,
+    DOP853,
+    LSODA,
+    RK23,
+    RK45,
+    DenseOutput,
+    OdeSolution,
+    OdeSolver,
+    Radau,
+    solve_ivp,
 )
 from ._ode import *
 from ._odepack_py import *
-from ._quad_vec import quad_vec as quad_vec
+from ._quad_vec import quad_vec
 from ._quadpack_py import *
 from ._quadrature import *
-from ._tanhsinh import nsum as nsum
+
+__all__ = ["dop", "lsoda", "odepack", "quadpack", "vode"]
+__all__ += [
+    "BDF",
+    "DOP853",
+    "LSODA",
+    "RK23",
+    "RK45",
+    "DenseOutput",
+    "OdeSolution",
+    "OdeSolver",
+    "Radau",
+    "newton_cotes",
+    "quad_vec",
+    "solve_bvp",
+    "solve_ivp",
+]
+__all__ += _ode.__all__
+__all__ += _odepack_py.__all__
+__all__ += _quadpack_py.__all__
+__all__ += _quadrature.__all__
