771e7db650d6a83ca4c5fc0b1df056971a9335cf
jorenham <jhammudoglu@gmail.com>
2024-09-04 14:06:21 +0200

    type-hint `scipy.integrate` public (deprecated) submodules

--- scipy-stubs/integrate/lsoda.pyi
+++ scipy-stubs/integrate/lsoda.pyi
@@ -1,4 +1,9 @@
-from scipy._typing import Untyped
+# This file is not meant for public use and will be removed in SciPy v2.0.0.
+from typing_extensions import Never, deprecated
 
-def __dir__() -> Untyped: ...
-def __getattr__(name) -> Untyped: ...
+__all__: list[Never] = []
+
+@deprecated("will be removed in SciPy 2.0.0.")
+def __dir__() -> list[Never]: ...
+@deprecated("will be removed in SciPy 2.0.0.")
+def __getattr__(name: str) -> Never: ...  # pyright: ignore[reportIncompleteStub]
