d3fa43976b3332b7bde2e4ee7669572f5b48f53f
jorenham <jhammudoglu@gmail.com>
2024-09-04 18:17:06 +0200

    type-hint `scipy.__init__`

--- scipy-stubs/__init__.pyi
+++ scipy-stubs/__init__.pyi
@@ -1,12 +1,82 @@
-from scipy._lib._ccallback import LowLevelCallable as LowLevelCallable
-from scipy._lib._testutils import PytestTester as PytestTester
-from scipy._typing import Untyped
+from collections.abc import Sequence
+from typing import Final, Literal
+from typing_extensions import LiteralString
 
-msg: str
-np_minversion: str
-np_maxversion: str
-test: Untyped
-submodules: Untyped
+from . import (
+    cluster,
+    constants,
+    datasets,
+    fft,
+    fftpack,
+    integrate,
+    interpolate,
+    io,
+    linalg,
+    misc,
+    ndimage,
+    odr,
+    optimize,
+    signal,
+    sparse,
+    spatial,
+    special,
+    stats,
+)
+from .__config__ import show as show_config
+from ._lib._ccallback import LowLevelCallable
+from ._lib._testutils import PytestTester
 
-def __dir__() -> Untyped: ...
-def __getattr__(name) -> Untyped: ...
+__all__ = [
+    "LowLevelCallable",
+    "__version__",
+    "cluster",
+    "constants",
+    "datasets",
+    "fft",
+    "fftpack",
+    "integrate",
+    "interpolate",
+    "io",
+    "linalg",
+    "misc",
+    "ndimage",
+    "odr",
+    "optimize",
+    "show_config",
+    "signal",
+    "sparse",
+    "spatial",
+    "special",
+    "stats",
+    "test",
+]
+__version__: Final[LiteralString]
+np_minversion: Final[LiteralString]
+np_maxversion: Final[LiteralString]
+
+test: Final[PytestTester]
+
+submodules: Final[
+    Sequence[
+        Literal[
+            "cluster",
+            "constants",
+            "datasets",
+            "fft",
+            "fftpack",
+            "integrate",
+            "interpolate",
+            "io",
+            "linalg",
+            "misc",
+            "ndimage",
+            "odr",
+            "optimize",
+            "signal",
+            "sparse",
+            "spatial",
+            "special",
+            "stats",
+        ]
+    ]
+]
