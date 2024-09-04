7469ac33f7ffb743c1ce9087c25cb47bed6959a5
jorenham <jhammudoglu@gmail.com>
2024-09-04 15:10:33 +0200

    type-hint `scipy.integrate._ivp.base`

--- scipy-stubs/integrate/_ivp/base.pyi
+++ scipy-stubs/integrate/_ivp/base.pyi
@@ -1,36 +1,62 @@
+from collections.abc import Callable, Sequence
+from typing import Any, ClassVar, Final, Generic, Literal, TypeAlias, TypeVar
+
+import numpy as np
+import numpy.typing as npt
+import optype.numpy as onpt
 from scipy._typing import Untyped
 
-def check_arguments(fun, y0, support_complex) -> Untyped: ...
+_VT = TypeVar("_VT", bound=npt.NDArray[np.inexact[Any]], default=npt.NDArray[np.inexact[Any]])
+
+_ArrayLikeReal: TypeAlias = float | Sequence[float] | onpt.AnyFloatingArray | onpt.AnyIntegerArray
+_ArrayLikeComplex: TypeAlias = complex | Sequence[complex] | onpt.AnyComplexFloatingArray
+
+def check_arguments(
+    fun: Callable[[float, npt.NDArray[np.float64]], _ArrayLikeComplex],
+    y0: _ArrayLikeComplex,
+    support_complex: bool,
+) -> (
+    Callable[[float, npt.NDArray[np.float64]], npt.NDArray[np.float64]]
+    | Callable[[float, npt.NDArray[np.float64]], npt.NDArray[np.complex128]]
+): ...
 
 class OdeSolver:
-    TOO_SMALL_STEP: str
-    t_old: Untyped
-    t: Untyped
-    t_bound: Untyped
-    vectorized: Untyped
-    fun: Untyped
-    fun_single: Untyped
-    fun_vectorized: Untyped
-    direction: Untyped
-    n: Untyped
-    status: str
+    TOO_SMALL_STEP: ClassVar[str]
+    t: float
+    t_old: float
+    t_bound: float
+    vectorized: bool
+    fun: Callable[[float, npt.NDArray[np.float64]], npt.NDArray[np.float64]]
+    fun_single: Callable[[float, npt.NDArray[np.float64]], npt.NDArray[np.float64]]
+    fun_vectorized: Callable[[float, npt.NDArray[np.float64]], npt.NDArray[np.float64]]
+    direction: float
+    n: int
+    status: Literal["running", "finished", "failed"]
     nfev: int
     njev: int
     nlu: int
-    def __init__(self, fun, t0, y0, t_bound, vectorized, support_complex: bool = False): ...
+    def __init__(
+        self,
+        fun: Callable[[float, npt.NDArray[np.float64]], _ArrayLikeComplex],
+        t0: float,
+        y0: _ArrayLikeComplex,
+        t_bound: float,
+        vectorized: bool,
+        support_complex: bool = False,
+    ) -> None: ...
     @property
-    def step_size(self) -> Untyped: ...
-    def step(self) -> Untyped: ...
-    def dense_output(self) -> Untyped: ...
+    def step_size(self) -> float | None: ...
+    def step(self) -> str | None: ...
+    def dense_output(self) -> ConstantDenseOutput: ...
 
 class DenseOutput:
-    t_old: Untyped
-    t: Untyped
-    t_min: Untyped
-    t_max: Untyped
-    def __init__(self, t_old, t) -> None: ...
-    def __call__(self, t) -> Untyped: ...
+    t_old: Final[float]
+    t: Final[float]
+    t_min: Final[float]
+    t_max: Final[float]
+    def __init__(self, t_old: float, t: float, /) -> None: ...
+    def __call__(self, /, t: _ArrayLikeReal) -> Untyped: ...
 
-class ConstantDenseOutput(DenseOutput):
+class ConstantDenseOutput(DenseOutput, Generic[_VT]):
     value: Untyped
-    def __init__(self, t_old, t, value) -> None: ...
+    def __init__(self, t_old: float, t: float, /, value: _VT) -> None: ...
