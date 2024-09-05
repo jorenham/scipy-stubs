b425bf9ddeb9cc12a561be191efe72611da1fb3c
jorenham <jhammudoglu@gmail.com>
2024-09-06 00:34:17 +0200

    fix stubtests and improve `scipy.linalg._decomp_lu`

--- scipy-stubs/linalg/_decomp_lu.pyi
+++ scipy-stubs/linalg/_decomp_lu.pyi
@@ -1,16 +1,25 @@
-from typing import Literal, TypeAlias, overload
+from collections.abc import Sequence
+from typing import Any, Literal, TypeAlias, overload
 
 import numpy as np
 import numpy.typing as npt
+import optype.numpy as onpt
 
 __all__ = ["lu", "lu_factor", "lu_solve"]
 
+_ArrayLike_2d_fc: TypeAlias = onpt.AnyNumberArray | Sequence[Sequence[complex | np.number[Any]]]
 _Array_i: TypeAlias = np.ndarray[tuple[int, ...], np.dtype[np.intp]]
 _Array_fc: TypeAlias = np.ndarray[tuple[int, ...], np.dtype[np.inexact[npt.NBitBase]]]
 _Array_fc_1d: TypeAlias = np.ndarray[tuple[int], np.dtype[np.inexact[npt.NBitBase]]]
 _Array_fc_2d: TypeAlias = np.ndarray[tuple[int, int], np.dtype[np.inexact[npt.NBitBase]]]
 
-def lu_factor(a: npt.ArrayLike, overwrite_a: bool = False, check_finite: bool = True) -> tuple[_Array_fc_2d, _Array_fc_1d]: ...
+def lu_factor(
+    a: _ArrayLike_2d_fc,
+    overwrite_a: bool = False,
+    check_finite: bool = True,
+) -> tuple[_Array_fc_2d, _Array_fc_1d]: ...
+
+#
 def lu_solve(
     lu_and_piv: tuple[_Array_fc_2d, _Array_fc_1d],
     b: npt.ArrayLike,
@@ -18,9 +27,11 @@ def lu_solve(
     overwrite_b: bool = False,
     check_finite: bool = True,
 ) -> _Array_fc_2d: ...
+
+#
 @overload
 def lu(
-    a: npt.ArrayLike,
+    a: _ArrayLike_2d_fc,
     permute_l: Literal[False, 0] = False,
     overwrite_a: bool = False,
     check_finite: bool = True,
@@ -28,7 +39,15 @@ def lu(
 ) -> tuple[_Array_fc, _Array_fc, _Array_fc]: ...
 @overload
 def lu(
-    a: npt.ArrayLike,
+    a: _ArrayLike_2d_fc,
+    permute_l: Literal[False],
+    overwrite_a: bool,
+    check_finite: bool,
+    p_indices: Literal[True],
+) -> tuple[_Array_i, _Array_fc, _Array_fc]: ...
+@overload
+def lu(
+    a: _ArrayLike_2d_fc,
     permute_l: Literal[False, 0] = False,
     overwrite_a: bool = False,
     check_finite: bool = True,
@@ -37,16 +56,7 @@ def lu(
 ) -> tuple[_Array_i, _Array_fc, _Array_fc]: ...
 @overload
 def lu(
-    a: npt.ArrayLike,
-    permute_l: Literal[False],
-    overwrite_a: bool,
-    check_finite: bool,
-    p_indices: Literal[True],
-    /,
-) -> tuple[_Array_i, _Array_fc, _Array_fc]: ...
-@overload
-def lu(
-    a: npt.ArrayLike,
+    a: _ArrayLike_2d_fc,
     permute_l: Literal[True],
     overwrite_a: bool = False,
     check_finite: bool = True,
