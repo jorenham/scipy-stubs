f3f304a54fe5b1537038647907979ce9ccbae05f
jorenham <jhammudoglu@gmail.com>
2024-09-05 17:05:14 +0200

    type-hint `scipy.datasets`

--- scipy-stubs/datasets/_registry.pyi
+++ scipy-stubs/datasets/_registry.pyi
@@ -1,5 +1,6 @@
-from scipy._typing import Untyped
+from typing import Final
+from typing_extensions import LiteralString
 
-registry: Untyped
-registry_urls: Untyped
-method_files_map: Untyped
+registry: Final[dict[LiteralString, LiteralString]]
+registry_urls: Final[dict[LiteralString, LiteralString]]
+method_files_map: Final[dict[LiteralString, list[LiteralString]]]
--- scipy-stubs/datasets/_utils.pyi
+++ scipy-stubs/datasets/_utils.pyi
@@ -1,4 +1,12 @@
-from scipy._typing import Untyped
-from ._registry import method_files_map as method_files_map
+from collections.abc import Callable
+from typing import TypeAlias
+from typing_extensions import TypeVar
 
-def clear_cache(datasets: Untyped | None = None): ...
+import numpy as np
+
+_ShapeT = TypeVar("_ShapeT", bound=tuple[int, ...], default=tuple[int] | tuple[int, int] | tuple[int, int, int])
+_DT = TypeVar("_DT", bound=np.dtype[np.generic], default=np.dtype[np.float64] | np.dtype[np.uint8])
+
+_AnyDataset: TypeAlias = Callable[[], np.ndarray[_ShapeT, _DT]]
+
+def clear_cache(datasets: list[_AnyDataset] | tuple[_AnyDataset, ...] | None = None) -> None: ...
