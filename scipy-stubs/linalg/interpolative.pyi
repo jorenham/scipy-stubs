28e172cd9b4ee4e485e2bf5b959907acb824b21f
jorenham <jhammudoglu@gmail.com>
2024-09-05 17:37:01 +0200

    fix stubtests for `scipy.linalg.interpolative`

--- scipy-stubs/linalg/interpolative.pyi
+++ scipy-stubs/linalg/interpolative.pyi
@@ -1,5 +1,5 @@
 from typing import SupportsIndex, TypeAlias, TypeVar
-from typing_extensions import Never, deprecated
+from typing_extensions import deprecated
 
 import numpy as np
 import numpy.typing as npt
@@ -24,24 +24,25 @@ _DT = TypeVar("_DT", bound=np.dtype[np.generic])
 _Array_fc_1d: TypeAlias = np.ndarray[tuple[int], np.dtype[np.inexact[npt.NBitBase]]]
 _Array_fc_2d: TypeAlias = np.ndarray[tuple[int, int], np.dtype[np.inexact[npt.NBitBase]]]
 
+_AnyNumber: TypeAlias = np.number[npt.NBitBase]
+
 @deprecated("will be removed in SciPy 1.17.0.")
-def seed(seed: Never = ...) -> None: ...
+def seed(seed: None = None) -> None: ...
 @deprecated("will be removed in SciPy 1.17.0.")
 def rand(*shape: int) -> npt.NDArray[np.float64]: ...
 def interp_decomp(
-    A: npt.NDArray[np.number[npt.NBitBase]] | LinearOperator,
+    A: npt.NDArray[_AnyNumber] | LinearOperator,
     eps_or_k: spt.AnyReal,
     rand: bool = True,
-    rng: np.random.Generator | None = None,
 ) -> tuple[int, npt.NDArray[np.intp], npt.NDArray[np.float64]]: ...
 def reconstruct_matrix_from_id(
     B: npt.NDArray[np.generic],
     idx: npt.NDArray[np.integer[npt.NBitBase]],
-    proj: npt.NDArray[np.number[npt.NBitBase]],
-) -> npt.NDArray[np.number[npt.NBitBase]]: ...
+    proj: npt.NDArray[_AnyNumber],
+) -> npt.NDArray[_AnyNumber]: ...
 def reconstruct_interp_matrix(
     idx: npt.NDArray[np.integer[npt.NBitBase]],
-    proj: npt.NDArray[np.number[npt.NBitBase]],
+    proj: npt.NDArray[_AnyNumber],
 ) -> npt.NDArray[np.float64 | np.complex128]: ...
 def reconstruct_skel_matrix(
     A: np.ndarray[tuple[int, ...], _DT],
@@ -51,23 +52,13 @@ def reconstruct_skel_matrix(
 def id_to_svd(
     B: npt.NDArray[np.generic],
     idx: npt.NDArray[np.integer[npt.NBitBase]],
-    proj: npt.NDArray[np.number[npt.NBitBase]],
+    proj: npt.NDArray[_AnyNumber],
 ) -> tuple[_Array_fc_2d, _Array_fc_1d, _Array_fc_2d]: ...
-def estimate_spectral_norm(A: LinearOperator, its: int = 20, rng: np.random.Generator | None = None) -> float | np.float64: ...
-def estimate_spectral_norm_diff(
-    A: LinearOperator,
-    B: LinearOperator,
-    its: int = 20,
-    rng: np.random.Generator | None = None,
-) -> float | np.float64: ...
+def estimate_spectral_norm(A: LinearOperator, its: int = 20) -> float | np.float64: ...
+def estimate_spectral_norm_diff(A: LinearOperator, B: LinearOperator, its: int = 20) -> float | np.float64: ...
 def svd(
-    A: npt.NDArray[np.number[npt.NBitBase]] | LinearOperator,
+    A: npt.NDArray[_AnyNumber] | LinearOperator,
     eps_or_k: spt.AnyReal,
     rand: bool = True,
-    rng: np.random.Generator | None = None,
 ) -> tuple[_Array_fc_2d, _Array_fc_1d, _Array_fc_2d]: ...
-def estimate_rank(
-    A: npt.NDArray[np.number[npt.NBitBase]] | LinearOperator,
-    eps: spt.AnyReal,
-    rng: np.random.Generator | None = None,
-) -> int: ...
+def estimate_rank(A: npt.NDArray[_AnyNumber] | LinearOperator, eps: spt.AnyReal) -> int: ...
