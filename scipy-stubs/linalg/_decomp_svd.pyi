7eb73ecae081b793ce2afdaee3f5c383fddb5350
jorenham <jhammudoglu@gmail.com>
2024-09-06 00:20:33 +0200

    fix stubtests and improve `scipy.linalg._decomp_svd`

--- scipy-stubs/linalg/_decomp_svd.pyi
+++ scipy-stubs/linalg/_decomp_svd.pyi
@@ -1,52 +1,106 @@
-from typing import Literal, TypeAlias, overload
+from collections.abc import Sequence
+from typing import Any, Literal, TypeAlias, overload
+from typing_extensions import TypeVar
 
 import numpy as np
-import numpy.typing as npt
-import scipy._typing as spt
+import optype as op
+import optype.numpy as onpt
 
 __all__ = ["diagsvd", "null_space", "orth", "subspace_angles", "svd", "svdvals"]
 
-_Array_f_1d: TypeAlias = np.ndarray[tuple[int], np.dtype[np.floating[npt.NBitBase]]]
-_Array_f_2d: TypeAlias = np.ndarray[tuple[int, int], np.dtype[np.floating[npt.NBitBase]]]
+_LapackDriver: TypeAlias = Literal["gesdd", "gesvd"]
+
+_SCT = TypeVar("_SCT", bound=np.generic)
+_DT = TypeVar("_DT", bound=np.dtype[np.generic])
+
+_Array_1d: TypeAlias = onpt.Array[tuple[int], _SCT]
+_Array_2d: TypeAlias = onpt.Array[tuple[int, int], _SCT]
+
+_ArrayLike_2d_f: TypeAlias = Sequence[Sequence[float]] | onpt.AnyFloatingArray
+_ArrayLike_2d_c: TypeAlias = onpt.AnyComplexFloatingArray  # because `float <: complex` (type-check only), complex is excluded
+_ArrayLike_2d_fc: TypeAlias = Sequence[Sequence[complex]] | onpt.AnyNumberArray
+
+_Array_1d_f: TypeAlias = _Array_1d[np.float32 | np.float64]
+_Array_2d_f: TypeAlias = _Array_2d[np.float32 | np.float64]
+_Array_2d_c: TypeAlias = _Array_2d[np.complex64 | np.complex128]
 
 @overload
 def svd(
-    a: npt.ArrayLike,
+    a: _ArrayLike_2d_f,
     full_matrices: bool = True,
     compute_uv: Literal[True] = True,
     overwrite_a: bool = False,
     check_finite: bool = True,
-    lapack_driver: Literal["gesdd", "gesvd"] = "gesdd",
-) -> tuple[_Array_f_2d, _Array_f_1d, _Array_f_2d]: ...
+    lapack_driver: _LapackDriver = "gesdd",
+) -> tuple[_Array_2d_f, _Array_1d_f, _Array_2d_f]: ...
 @overload
 def svd(
-    a: npt.ArrayLike,
+    a: _ArrayLike_2d_c,
+    full_matrices: bool = True,
+    compute_uv: Literal[True] = True,
+    overwrite_a: bool = False,
+    check_finite: bool = True,
+    lapack_driver: _LapackDriver = "gesdd",
+) -> tuple[_Array_2d_c, _Array_1d_f, _Array_2d_c]: ...
+@overload
+def svd(
+    a: _ArrayLike_2d_fc,
+    full_matrices: bool = True,
+    compute_uv: Literal[True] = True,
+    overwrite_a: bool = False,
+    check_finite: bool = True,
+    lapack_driver: _LapackDriver = "gesdd",
+) -> tuple[_Array_2d_f, _Array_1d_f, _Array_2d_f] | tuple[_Array_2d_c, _Array_1d_f, _Array_2d_c]: ...
+@overload
+def svd(
+    a: _ArrayLike_2d_fc,
+    full_matrices: bool,
+    compute_uv: Literal[False],
+    overwrite_a: bool = False,
+    check_finite: bool = True,
+    lapack_driver: _LapackDriver = "gesdd",
+) -> _Array_1d_f: ...
+@overload
+def svd(
+    a: _ArrayLike_2d_fc,
     full_matrices: bool = True,
     *,
     compute_uv: Literal[False],
     overwrite_a: bool = False,
     check_finite: bool = True,
-    lapack_driver: Literal["gesdd", "gesvd"] = "gesdd",
-) -> _Array_f_1d: ...
+    lapack_driver: _LapackDriver = "gesdd",
+) -> _Array_1d_f: ...
+
+#
+def svdvals(a: _ArrayLike_2d_fc, overwrite_a: bool = False, check_finite: bool = True) -> _Array_1d_f: ...
+
+# beware the overlapping overloads for bool <: int (<: float)
 @overload
-def svd(
-    a: npt.ArrayLike,
-    full_matrices: bool,
-    compute_uv: Literal[False],
-    /,
-    overwrite_a: bool = False,
-    check_finite: bool = True,
-    lapack_driver: Literal["gesdd", "gesvd"] = "gesdd",
-) -> _Array_f_1d: ...
-def svdvals(a: npt.ArrayLike, overwrite_a: bool = False, check_finite: bool = True) -> _Array_f_1d: ...
-def diagsvd(s: npt.ArrayLike, M: spt.AnyInt, N: spt.AnyInt) -> _Array_f_2d: ...
-def orth(A: npt.ArrayLike, rcond: spt.AnyReal | None = None) -> _Array_f_2d: ...
-def null_space(
-    A: npt.ArrayLike,
-    rcond: spt.AnyReal | None = None,
-    *,
-    overwrite_a: bool = False,
-    check_finite: bool = True,
-    lapack_driver: Literal["gesdd", "gesvd"] = "gesdd",
-) -> _Array_f_2d: ...
-def subspace_angles(A: npt.ArrayLike, B: npt.ArrayLike) -> _Array_f_1d: ...
+def diagsvd(s: onpt.CanArray[tuple[int, ...], _DT], M: op.CanIndex, N: op.CanIndex) -> np.ndarray[tuple[int, int], _DT]: ...
+@overload
+def diagsvd(s: Sequence[_SCT], M: op.CanIndex, N: op.CanIndex) -> _Array_2d[_SCT]: ...
+@overload
+def diagsvd(s: Sequence[bool], M: op.CanIndex, N: op.CanIndex) -> _Array_2d[np.bool_]: ...
+@overload
+def diagsvd(s: Sequence[int], M: op.CanIndex, N: op.CanIndex) -> _Array_2d[np.bool_ | np.int_]: ...
+@overload
+def diagsvd(s: Sequence[float], M: op.CanIndex, N: op.CanIndex) -> _Array_2d[np.bool_ | np.int_ | np.float64]: ...
+
+#
+@overload
+def orth(A: _ArrayLike_2d_f, rcond: float | np.floating[Any] | None = None) -> _Array_2d_f: ...
+@overload
+def orth(A: _ArrayLike_2d_c, rcond: float | np.floating[Any] | None = None) -> _Array_2d_c: ...
+@overload
+def orth(A: _ArrayLike_2d_fc, rcond: float | np.floating[Any] | None = None) -> _Array_2d_f | _Array_2d_c: ...
+
+#
+@overload
+def null_space(A: _ArrayLike_2d_f, rcond: float | np.floating[Any] | None = None) -> _Array_2d_f: ...
+@overload
+def null_space(A: _ArrayLike_2d_c, rcond: float | np.floating[Any] | None = None) -> _Array_2d_c: ...
+@overload
+def null_space(A: _ArrayLike_2d_fc, rcond: float | np.floating[Any] | None = None) -> _Array_2d_f | _Array_2d_c: ...
+
+#
+def subspace_angles(A: _ArrayLike_2d_fc, B: _ArrayLike_2d_fc) -> _Array_1d_f: ...
