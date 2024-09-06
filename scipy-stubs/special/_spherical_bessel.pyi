ffbfb8e63f004fd4f800df0e0dbaaa5eaaf3a2c6
jorenham <jhammudoglu@gmail.com>
2024-09-07 01:02:55 +0200

    improved `scipy.special._spherical_bessel` type-hints

--- scipy-stubs/special/_spherical_bessel.pyi
+++ scipy-stubs/special/_spherical_bessel.pyi
@@ -2,10 +2,27 @@ from typing import TypeAlias
 
 import numpy as np
 import numpy.typing as npt
+from numpy._typing import _ArrayLikeComplex_co, _ArrayLikeInt_co
 
 _Scalar_fc: TypeAlias = np.float64 | np.complex128
 
-def spherical_jn(n: npt.ArrayLike, z: npt.ArrayLike, derivative: bool = ...) -> _Scalar_fc | npt.NDArray[_Scalar_fc]: ...
-def spherical_yn(n: npt.ArrayLike, z: npt.ArrayLike, derivative: bool = ...) -> _Scalar_fc | npt.NDArray[_Scalar_fc]: ...
-def spherical_in(n: npt.ArrayLike, z: npt.ArrayLike, derivative: bool = ...) -> _Scalar_fc | npt.NDArray[_Scalar_fc]: ...
-def spherical_kn(n: npt.ArrayLike, z: npt.ArrayLike, derivative: bool = ...) -> _Scalar_fc | npt.NDArray[_Scalar_fc]: ...
+def spherical_jn(
+    n: _ArrayLikeInt_co,
+    z: _ArrayLikeComplex_co,
+    derivative: bool = False,
+) -> _Scalar_fc | npt.NDArray[_Scalar_fc]: ...
+def spherical_yn(
+    n: _ArrayLikeInt_co,
+    z: _ArrayLikeComplex_co,
+    derivative: bool = False,
+) -> _Scalar_fc | npt.NDArray[_Scalar_fc]: ...
+def spherical_in(
+    n: _ArrayLikeInt_co,
+    z: _ArrayLikeComplex_co,
+    derivative: bool = False,
+) -> _Scalar_fc | npt.NDArray[_Scalar_fc]: ...
+def spherical_kn(
+    n: _ArrayLikeInt_co,
+    z: _ArrayLikeComplex_co,
+    derivative: bool = False,
+) -> _Scalar_fc | npt.NDArray[_Scalar_fc]: ...
