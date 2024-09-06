9b32e3fc30f86262acf39632607ea55eeec7e7e9
jorenham <jhammudoglu@gmail.com>
2024-09-07 01:05:16 +0200

    improved `scipy.special._spfun_stats` type-hints

--- scipy-stubs/special/_spfun_stats.pyi
+++ scipy-stubs/special/_spfun_stats.pyi
@@ -1,6 +1,8 @@
 import numpy as np
 import numpy.typing as npt
+from numpy._typing import _ArrayLikeFloat_co
+import scipy._typing as spt
 
 __all__ = ["multigammaln"]
 
-def multigammaln(a: npt.ArrayLike, d: int) -> np.float64 | npt.NDArray[np.float64]: ...
+def multigammaln(a: _ArrayLikeFloat_co, d: spt.AnyInt) -> np.float64 | npt.NDArray[np.float64]: ...
