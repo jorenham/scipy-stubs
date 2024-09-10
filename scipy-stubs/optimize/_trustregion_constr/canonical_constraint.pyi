94cb93cdd16a758f77f80314fc67be3294c4bab6
jorenham <jhammudoglu@gmail.com>
2024-09-10 13:18:45 +0200

    fix pyright errors in `scipy.optimize._trustregion_constr`

--- scipy-stubs/optimize/_trustregion_constr/canonical_constraint.pyi
+++ scipy-stubs/optimize/_trustregion_constr/canonical_constraint.pyi
@@ -1,18 +1,59 @@
+from collections.abc import Callable
+from typing import TypeAlias, TypeVar
+from typing_extensions import Self
+
+import numpy as np
+import optype as op
+import optype.numpy as onpt
 from scipy._typing import Untyped
+from scipy.optimize._constraints import PreparedConstraint
+from scipy.sparse import csr_matrix
+from scipy.sparse.linalg._interface import LinearOperator
+
+_T = TypeVar("_T")
+_Tuple2: TypeAlias = tuple[_T, _T]
+
+_FunConstr: TypeAlias = Callable[[onpt.Array[tuple[int], np.float64]], _Tuple2[onpt.Array[tuple[int], np.float64]]]
+_FunJac: TypeAlias = Callable[[onpt.Array[tuple[int], np.float64]], _Tuple2[onpt.Array[tuple[int, int], np.float64] | csr_matrix]]
+_FunHess: TypeAlias = Callable[
+    [onpt.Array[tuple[int], np.float64], onpt.Array[tuple[int], np.float64], onpt.Array[tuple[int], np.float64]],
+    _Tuple2[onpt.Array[tuple[int, int], np.float64] | csr_matrix | LinearOperator],
+]
+
+# tighter than `Iterable[PreparedConstraint]` ;)
+_PreparedConstraints: TypeAlias = op.CanIter[op.CanNext[CanonicalConstraint]]
 
 class CanonicalConstraint:
-    n_eq: Untyped
-    n_ineq: Untyped
+    n_eq: int
+    n_ineq: int
     fun: Untyped
     jac: Untyped
     hess: Untyped
     keep_feasible: Untyped
-    def __init__(self, n_eq, n_ineq, fun, jac, hess, keep_feasible) -> None: ...
+    def __init__(
+        self,
+        /,
+        n_eq: int,
+        n_ineq: int,
+        fun: _FunConstr,
+        jac: _FunJac,
+        hess: _FunHess,
+        keep_feasible: onpt.Array[tuple[int], np.bool_],
+    ) -> None: ...
     @classmethod
-    def from_PreparedConstraint(cls, constraint) -> Untyped: ...
+    def from_PreparedConstraint(cls, constraint: PreparedConstraint) -> Self: ...
     @classmethod
-    def empty(cls, n) -> Untyped: ...
+    def empty(cls, n: op.CanIndex) -> Self: ...
     @classmethod
-    def concatenate(cls, canonical_constraints, sparse_jacobian) -> Untyped: ...
+    def concatenate(cls, canonical_constraints: _PreparedConstraints, sparse_jacobian: bool | np.bool_) -> Self: ...
 
-def initial_constraints_as_canonical(n, prepared_constraints, sparse_jacobian) -> Untyped: ...
+def initial_constraints_as_canonical(
+    n: op.CanIndex,
+    prepared_constraints: _PreparedConstraints,
+    sparse_jacobian: bool | np.bool_,
+) -> tuple[
+    onpt.Array[onpt.AtMost2D, np.float64],
+    onpt.Array[onpt.AtMost2D, np.float64],
+    onpt.Array[tuple[int, int], np.float64] | csr_matrix,
+    onpt.Array[tuple[int, int], np.float64] | csr_matrix,
+]: ...
--- scipy-stubs/optimize/_trustregion_constr/qp_subproblem.pyi
+++ scipy-stubs/optimize/_trustregion_constr/qp_subproblem.pyi
@@ -1,24 +1,129 @@
-from scipy._typing import Untyped
-from scipy.sparse import bmat as bmat, csc_matrix as csc_matrix, linalg as linalg
+from collections.abc import Sequence
+from typing import Any, Literal, TypeAlias, TypedDict, overload
+from typing_extensions import NotRequired, TypeVar
 
-def eqp_kktfact(H, c, A, b) -> Untyped: ...
-def sphere_intersections(z, d, trust_radius, entire_line: bool = False) -> Untyped: ...
-def box_intersections(z, d, lb, ub, entire_line: bool = False) -> Untyped: ...
-def box_sphere_intersections(z, d, lb, ub, trust_radius, entire_line: bool = False, extra_info: bool = False) -> Untyped: ...
-def inside_box_boundaries(x, lb, ub) -> Untyped: ...
-def reinforce_box_boundaries(x, lb, ub) -> Untyped: ...
-def modified_dogleg(A, Y, b, trust_radius, lb, ub) -> Untyped: ...
+import numpy as np
+import optype.numpy as onpt
+from scipy.sparse import sparray, spmatrix
+from scipy.sparse.linalg import LinearOperator
+
+__all__ = [
+    "box_intersections",
+    "box_sphere_intersections",
+    "eqp_kktfact",
+    "inside_box_boundaries",
+    "modified_dogleg",
+    "projected_cg",
+    "sphere_intersections",
+]
+
+_ScalarB1: TypeAlias = bool | np.bool_
+_ScalarF8: TypeAlias = float | np.float64
+_VectorF8: TypeAlias = onpt.Array[tuple[int], np.float64]
+
+_ScalarInt_co: TypeAlias = np.integer[Any]
+_ScalarFloat_co: TypeAlias = np.floating[Any] | _ScalarInt_co
+
+_ScalarLikeInt_co: TypeAlias = int | _ScalarInt_co
+_ScalarLikeFloat_co: TypeAlias = float | _ScalarFloat_co
+_VectorLikeFloat_co: TypeAlias = Sequence[_ScalarLikeFloat_co] | onpt.CanArray[tuple[int], np.dtype[_ScalarFloat_co]]
+
+_ShapeT = TypeVar("_ShapeT", bound=tuple[int, ...])
+_SCT_float = TypeVar("_SCT_float", bound=_ScalarFloat_co)
+
+_SparseArray: TypeAlias = sparray | spmatrix
+
+class _SphereInfoDict(TypedDict):
+    ta: _ScalarF8
+    tb: _ScalarF8
+    intersect: bool
+
+class _ProjectedCGDict(TypedDict):
+    niter: int
+    stop_cond: Literal[1, 2, 3, 4]
+    hits_boundary: bool
+    allvecs: NotRequired[Sequence[_VectorF8]]
+
+def eqp_kktfact(
+    H: _SparseArray,
+    c: _VectorLikeFloat_co,
+    A: _SparseArray,
+    b: _VectorLikeFloat_co,
+) -> tuple[_VectorF8, _VectorF8]: ...
+def sphere_intersections(
+    z: _VectorLikeFloat_co,
+    d: _VectorLikeFloat_co,
+    trust_radius: _ScalarLikeFloat_co,
+    entire_line: _ScalarB1 = False,
+) -> tuple[_ScalarF8, _ScalarF8, _ScalarB1]: ...
+def box_intersections(
+    z: _VectorLikeFloat_co,
+    d: _VectorLikeFloat_co,
+    lb: _VectorLikeFloat_co,
+    ub: _VectorLikeFloat_co,
+    entire_line: _ScalarB1 = False,
+) -> tuple[_ScalarF8, _ScalarF8, _ScalarB1]: ...
+@overload
+def box_sphere_intersections(
+    z: _VectorLikeFloat_co,
+    d: _VectorLikeFloat_co,
+    lb: _VectorLikeFloat_co,
+    ub: _VectorLikeFloat_co,
+    trust_radius: _ScalarLikeFloat_co,
+    entire_line: _ScalarB1 = False,
+    extra_info: Literal[False, 0, None] = False,
+) -> tuple[_ScalarF8, _ScalarF8, _ScalarB1]: ...
+@overload
+def box_sphere_intersections(
+    z: _VectorLikeFloat_co,
+    d: _VectorLikeFloat_co,
+    lb: _VectorLikeFloat_co,
+    ub: _VectorLikeFloat_co,
+    trust_radius: _ScalarLikeFloat_co,
+    entire_line: _ScalarB1,
+    extra_info: Literal[True, 1],
+) -> tuple[_ScalarF8, _ScalarF8, _ScalarB1, _SphereInfoDict, _SphereInfoDict]: ...
+@overload
+def box_sphere_intersections(
+    z: _VectorLikeFloat_co,
+    d: _VectorLikeFloat_co,
+    lb: _VectorLikeFloat_co,
+    ub: _VectorLikeFloat_co,
+    trust_radius: _ScalarLikeFloat_co,
+    entire_line: _ScalarB1 = False,
+    *,
+    extra_info: Literal[True, 1],
+) -> tuple[_ScalarF8, _ScalarF8, _ScalarB1, _SphereInfoDict, _SphereInfoDict]: ...
+
+def inside_box_boundaries(
+    x: onpt.Array[_ShapeT, _ScalarFloat_co],
+    lb: onpt.Array[_ShapeT, _ScalarFloat_co],
+    ub: onpt.Array[_ShapeT, _ScalarFloat_co],
+) -> np.bool_: ...
+def reinforce_box_boundaries(
+    x: onpt.Array[_ShapeT, _SCT_float],
+    lb: onpt.Array[_ShapeT, _SCT_float],
+    ub: onpt.Array[_ShapeT, _SCT_float],
+) -> onpt.Array[_ShapeT, _SCT_float]: ...
+def modified_dogleg(
+    A: LinearOperator | _SparseArray | onpt.Array[tuple[int, ...], _ScalarFloat_co],
+    Y: LinearOperator | _SparseArray | onpt.Array[tuple[int, ...], _ScalarFloat_co],
+    b: _VectorLikeFloat_co,
+    trust_radius: _ScalarLikeFloat_co,
+    lb: _VectorLikeFloat_co,
+    ub: _VectorLikeFloat_co,
+) -> _VectorF8: ...
 def projected_cg(
-    H,
-    c,
-    Z,
-    Y,
-    b,
-    trust_radius=...,
-    lb: Untyped | None = None,
-    ub: Untyped | None = None,
-    tol: Untyped | None = None,
-    max_iter: Untyped | None = None,
-    max_infeasible_iter: Untyped | None = None,
-    return_all: bool = False,
-) -> Untyped: ...
+    H: LinearOperator | _SparseArray | onpt.Array[tuple[int, ...], _ScalarFloat_co],
+    c: _VectorLikeFloat_co,
+    Z: LinearOperator | _SparseArray | onpt.Array[tuple[int, ...], _ScalarFloat_co],
+    Y: LinearOperator | _SparseArray | onpt.Array[tuple[int, ...], _ScalarFloat_co],
+    b: _VectorLikeFloat_co,
+    trust_radius: _ScalarLikeFloat_co = ...,
+    lb: _ScalarLikeFloat_co | None = None,
+    ub: _ScalarLikeFloat_co | None = None,
+    tol: _ScalarLikeFloat_co | None = None,
+    max_iter: _ScalarLikeInt_co | None = None,
+    max_infeasible_iter: _ScalarLikeInt_co | np.integer[Any] | None = None,
+    return_all: _ScalarB1 = False,
+) -> tuple[_VectorF8, _ProjectedCGDict]: ...
