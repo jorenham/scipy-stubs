d2410fbe18bbfd105ddc970908e3534e93294159
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-10-05 18:16:21 +1000

    `fftpack`: Add re-exported functions to `__all__` for public private submodules

--- scipy-stubs/fftpack/basic.pyi
+++ scipy-stubs/fftpack/basic.pyi
@@ -1,4 +1,6 @@
-from scipy._typing import Untyped
+# This module is not meant for public use and will be removed in SciPy v2.0.0.
+# This stub simply re-exports the imported functions.
+# TODO: Add type annotated dummy functions marked deprecated.
+from ._basic import *
 
-def __dir__() -> Untyped: ...
-def __getattr__(name) -> Untyped: ...
+__all__ = ["fft", "fft2", "fftn", "ifft", "ifft2", "ifftn", "irfft", "rfft"]
--- scipy-stubs/fftpack/pseudo_diffs.pyi
+++ scipy-stubs/fftpack/pseudo_diffs.pyi
@@ -1,4 +1,7 @@
-from scipy._typing import Untyped
+# This module is not meant for public use and will be removed in SciPy v2.0.0.
+# This stub simply re-exports the imported functions.
+# TODO: Add type annotated dummy functions marked deprecated.
+from . import convolve
+from ._pseudo_diffs import *
 
-def __dir__() -> Untyped: ...
-def __getattr__(name) -> Untyped: ...
+__all__ = ["cc_diff", "convolve", "cs_diff", "diff", "hilbert", "ihilbert", "itilbert", "sc_diff", "shift", "ss_diff", "tilbert"]
