6eb2c41f3aebfceb20f997753bae048c2d232ec0
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-10-08 01:20:10 +1100

    `sparse.linalg`: Add dummy deprecated functions/classes

--- scipy-stubs/sparse/linalg/dsolve.pyi
+++ scipy-stubs/sparse/linalg/dsolve.pyi
@@ -1,6 +1,70 @@
 # This module is not meant for public use and will be removed in SciPy v2.0.0.
-# This stub simply re-exports the imported functions.
-# TODO: Add type annotated dummy functions marked deprecated.
-from ._dsolve import *
+from types import ModuleType
+from typing import final
+from typing_extensions import deprecated
 
-__all__ = ["MatrixRankWarning", "SuperLU", "factorized", "spilu", "splu", "spsolve", "spsolve_triangular", "use_solver"]
+__all__ = [
+    "MatrixRankWarning",
+    "SuperLU",
+    "factorized",
+    "spilu",
+    "splu",
+    "spsolve",
+    "spsolve_triangular",
+    "test",
+    "use_solver",
+]
+
+test: ModuleType
+
+@deprecated("will be removed in SciPy v2.0.0")
+class MatrixRankWarning: ...
+
+@final
+@deprecated("will be removed in SciPy v2.0.0")
+class SuperLU:
+    L: object
+    U: object
+    nnz: object
+    perm_r: object
+    perm_c: object
+    shape: object
+
+    def solve(self, /, rhs: object) -> None: ...
+
+@deprecated("will be removed in SciPy v2.0.0")
+def use_solver(**kwargs: object) -> None: ...
+@deprecated("will be removed in SciPy v2.0.0")
+def spsolve(A: object, b: object, permc_spec: object = ..., use_umfpack: object = ...) -> object: ...
+@deprecated("will be removed in SciPy v2.0.0")
+def splu(
+    A: object,
+    permc_spec: object = ...,
+    diag_pivot_thresh: object = ...,
+    relax: object = ...,
+    panel_size: object = ...,
+    options: object = ...,
+) -> object: ...
+@deprecated("will be removed in SciPy v2.0.0")
+def spilu(
+    A: object,
+    drop_tol: object = ...,
+    fill_factor: object = ...,
+    drop_rule: object = ...,
+    permc_spec: object = ...,
+    diag_pivot_thresh: object = ...,
+    relax: object = ...,
+    panel_size: object = ...,
+    options: object = ...,
+) -> object: ...
+@deprecated("will be removed in SciPy v2.0.0")
+def factorized(A: object) -> object: ...
+@deprecated("will be removed in SciPy v2.0.0")
+def spsolve_triangular(
+    A: object,
+    b: object,
+    lower: object = ...,
+    overwrite_A: object = ...,
+    overwrite_b: object = ...,
+    unit_diagonal: object = ...,
+) -> object: ...
--- scipy-stubs/sparse/linalg/eigen.pyi
+++ scipy-stubs/sparse/linalg/eigen.pyi
@@ -1,6 +1,77 @@
 # This module is not meant for public use and will be removed in SciPy v2.0.0.
-# This stub simply re-exports the imported functions.
-# TODO: Add type annotated dummy functions marked deprecated.
-from ._eigen import *
+from types import ModuleType
+from typing_extensions import deprecated
 
-__all__ = ["ArpackError", "ArpackError", "ArpackNoConvergence", "eigs", "eigsh", "lobpcg", "svds"]
+__all__ = ["ArpackError", "ArpackNoConvergence", "eigs", "eigsh", "lobpcg", "svds", "test"]
+
+test: ModuleType
+
+@deprecated("will be removed in SciPy v2.0.0")
+class ArpackError:
+    def __init__(self, info: object, infodict: object = ...) -> None: ...
+
+@deprecated("will be removed in SciPy v2.0.0")
+class ArpackNoConvergence:
+    def __init__(self, msg: object, eigenvalues: object, eigenvectors: object) -> None: ...
+
+@deprecated("will be removed in SciPy v2.0.0")
+def eigs(
+    A: object,
+    k: object = ...,
+    M: object = ...,
+    sigma: object = ...,
+    which: object = ...,
+    v0: object = ...,
+    ncv: object = ...,
+    maxiter: object = ...,
+    tol: object = ...,
+    return_eigenvectors: object = ...,
+    Minv: object = ...,
+    OPinv: object = ...,
+    OPpart: object = ...,
+) -> object: ...
+@deprecated("will be removed in SciPy v2.0.0")
+def eigsh(
+    A: object,
+    k: object = ...,
+    M: object = ...,
+    sigma: object = ...,
+    which: object = ...,
+    v0: object = ...,
+    ncv: object = ...,
+    maxiter: object = ...,
+    tol: object = ...,
+    return_eigenvectors: object = ...,
+    Minv: object = ...,
+    OPinv: object = ...,
+    mode: object = ...,
+) -> object: ...
+@deprecated("will be removed in SciPy v2.0.0")
+def lobpcg(
+    A: object,
+    X: object,
+    B: object = ...,
+    M: object = ...,
+    Y: object = ...,
+    tol: object = ...,
+    maxiter: object = ...,
+    largest: object = ...,
+    verbosityLevel: object = ...,
+    retLambdaHistory: object = ...,
+    retResidualNormsHistory: object = ...,
+    restartControl: object = ...,
+) -> object: ...
+@deprecated("will be removed in SciPy v2.0.0")
+def svds(
+    A: object,
+    k: object = ...,
+    ncv: object = ...,
+    tol: object = ...,
+    which: object = ...,
+    v0: object = ...,
+    maxiter: object = ...,
+    return_singular_vectors: object = ...,
+    solver: object = ...,
+    random_state: object = ...,
+    options: object = ...,
+) -> object: ...
