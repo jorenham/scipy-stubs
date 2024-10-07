909a1bc41d3ad390119d7e8d504b9bb601e85a2c
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-10-07 15:44:21 +1100

    bug_fix: Add a dummy version of `convolve`
    This is to be able to export out `convolve` without needing the Cython
    module `convolve.pyx` to have type stubs.

--- scipy-stubs/fftpack/pseudo_diffs.pyi
+++ scipy-stubs/fftpack/pseudo_diffs.pyi
@@ -1,7 +1,11 @@
 # This module is not meant for public use and will be removed in SciPy v2.0.0.
 # This stub simply re-exports the imported functions.
 # TODO: Add type annotated dummy functions marked deprecated.
-from . import convolve
+from typing_extensions import deprecated
+
 from ._pseudo_diffs import *
+
+@deprecated("will be removed in SciPy v2.0.0")
+def convolve(*args: object, **kwargs: object) -> object: ...
 
 __all__ = ["cc_diff", "convolve", "cs_diff", "diff", "hilbert", "ihilbert", "itilbert", "sc_diff", "shift", "ss_diff", "tilbert"]
