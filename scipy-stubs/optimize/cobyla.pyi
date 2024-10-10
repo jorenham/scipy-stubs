9e50632702a3542fd7a587a749faa94251333a96
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-10-10 23:10:16 +1100

    `optimize`: Add deprecated dummy stub for `cobyla`

--- scipy-stubs/optimize/cobyla.pyi
+++ scipy-stubs/optimize/cobyla.pyi
@@ -1,6 +1,24 @@
 # This file is not meant for public use and will be removed in SciPy v2.0.0.
 
-from ._cobyla_py import fmin_cobyla
-from ._optimize import OptimizeResult
+from typing_extensions import deprecated
 
 __all__ = ["OptimizeResult", "fmin_cobyla"]
+
+@deprecated("will be removed in SciPy v2.0.0")
+class OptimizeResult: ...
+
+@deprecated("will be removed in SciPy v2.0.0")
+def fmin_cobyla(
+    func: object,
+    x0: object,
+    cons: object,
+    args: object = ...,
+    consargs: object = ...,
+    rhobeg: object = ...,
+    rhoend: object = ...,
+    maxfun: object = ...,
+    disp: object = ...,
+    catol: object = ...,
+    *,
+    callback: object = ...,
+) -> object: ...
