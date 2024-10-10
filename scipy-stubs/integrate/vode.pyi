51b05f51bbf5729531507269d1a1f26c93827ba9
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-10-10 19:33:40 +1100

    `integrate`: Remove `__getattr__` used in deprecated public modules
    Stubbing out `__getattr__` may confuse type checkers and in the case of
    these two files is entirely unnecessary as they do not export anything
    anyway.

--- scipy-stubs/integrate/vode.pyi
+++ scipy-stubs/integrate/vode.pyi
@@ -1,9 +1,3 @@
 # This file is not meant for public use and will be removed in SciPy v2.0.0.
-from typing_extensions import Never, deprecated
 
-__all__: list[Never] = []
-
-@deprecated("will be removed in SciPy 2.0.0.")
-def __dir__() -> list[Never]: ...
-@deprecated("will be removed in SciPy 2.0.0.")
-def __getattr__(name: str) -> Never: ...  # pyright: ignore[reportIncompleteStub]
+__all__: list[str] = []
