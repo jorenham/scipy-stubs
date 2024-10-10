bc5f1faf8bc71541a004cf72b769c9b0352f83e5
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-10-10 22:23:58 +1100

    `special`: Add deprecated dummy stub for specfun

--- scipy-stubs/special/specfun.pyi
+++ scipy-stubs/special/specfun.pyi
@@ -1,13 +1,20 @@
 # This file is not meant for public use and will be removed in SciPy v2.0.0.
 
-import numpy as np
-import optype.numpy as onpt
-from ._basic import clpmn, lpmn, lpn, lqmn
+from typing_extensions import deprecated
 
 __all__ = ["clpmn", "lpmn", "lpn", "lqmn", "pbdv"]
 
 # originally defined in scipy/special/_specfun.pyx
+@deprecated("will be removed in SciPy v2.0.0")
 def pbdv(
-    v: float | np.float64,
-    x: float | np.float64,
-) -> tuple[onpt.Array[tuple[int], np.float64], onpt.Array[tuple[int], np.float64], float, float]: ...
+    v: object,
+    x: object,
+) -> object: ...
+@deprecated("will be removed in SciPy v2.0.0")
+def clpmn(m: object, n: object, z: object, type: object = ...) -> object: ...
+@deprecated("will be removed in SciPy v2.0.0")
+def lpmn(m: object, n: object, z: object) -> object: ...
+@deprecated("will be removed in SciPy v2.0.0")
+def lpn(n: object, z: object) -> object: ...
+@deprecated("will be removed in SciPy v2.0.0")
+def lqmn(m: object, n: object, z: object) -> object: ...
