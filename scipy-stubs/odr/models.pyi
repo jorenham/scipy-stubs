729af3f21ddd9fa354ce6333b9389a59afaac1a9
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-10-10 23:59:21 +1100

    `odr`: Add deprecated dummy stub for `models`

--- scipy-stubs/odr/models.pyi
+++ scipy-stubs/odr/models.pyi
@@ -1,6 +1,28 @@
 # This module is not meant for public use and will be removed in SciPy v2.0.0.
-# This stub simply re-exports the imported functions.
-# TODO: Add type annotated dummy functions marked deprecated.
-from ._models import *
+from typing import Final
+from typing_extensions import deprecated
 
 __all__ = ["Model", "exponential", "multilinear", "polynomial", "quadratic", "unilinear"]
+
+@deprecated("will be removed in SciPy v2.0.0")
+class Model:
+    def __init__(
+        self,
+        fcn: object,
+        fjacb: object = ...,
+        fjacd: object = ...,
+        extra_args: object = ...,
+        estimate: object = ...,
+        implicit: object = ...,
+        meta: object = ...,
+    ) -> None: ...
+    def set_meta(self, **kwds: object) -> None: ...
+    def __getattr__(self, attr: object) -> object: ...
+
+@deprecated("will be removed in SciPy v2.0.0")
+def polynomial(order: object) -> object: ...
+
+multilinear: Final[object]
+exponential: Final[object]
+unilinear: Final[object]
+quadratic: Final[object]
