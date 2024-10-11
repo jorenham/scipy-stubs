ccb494e5f82c0476698962ed7896895a2e1ee0f6
jorenham <jhammudoglu@gmail.com>
2024-10-11 05:52:49 +0200

    fix stubtest & pyright errors in `scipy.spatial`

--- scipy-stubs/spatial/qhull.pyi
+++ scipy-stubs/spatial/qhull.pyi
@@ -1,8 +1,6 @@
 # This file is not meant for public use and will be removed in SciPy v2.0.0.
 # Use the `scipy.spatial` namespace for importing the functions
 # included below.
-from . import _qhull
 from ._qhull import *
 
-__all__: list[str] = []
-__all__ += _qhull.__all__
+__all__ = ["ConvexHull", "Delaunay", "HalfspaceIntersection", "QhullError", "Voronoi", "tsearch"]
