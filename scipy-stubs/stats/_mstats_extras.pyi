9adabbec1d5e5f3bc4935ada0181557d20138054
jorenham <jhammudoglu@gmail.com>
2024-10-12 03:23:46 +0200

    towards fixing `scipy.stats.mstats` stubtests

--- scipy-stubs/stats/_mstats_extras.pyi
+++ scipy-stubs/stats/_mstats_extras.pyi
@@ -1,11 +1,27 @@
 from scipy._typing import Untyped
-from scipy.stats.distributions import beta as beta, binom as binom, norm as norm, t as t
+
+__all__ = [
+    "compare_medians_ms",
+    "hdmedian",
+    "hdquantiles",
+    "hdquantiles_sd",
+    "idealfourths",
+    "median_cihs",
+    "mjci",
+    "mquantiles_cimj",
+    "rsh",
+    "trimmed_mean_ci",
+]
 
 def hdquantiles(data, prob=(0.25, 0.5, 0.75), axis: Untyped | None = None, var: bool = False) -> Untyped: ...
 def hdmedian(data, axis: int = -1, var: bool = False) -> Untyped: ...
 def hdquantiles_sd(data, prob=(0.25, 0.5, 0.75), axis: Untyped | None = None) -> Untyped: ...
 def trimmed_mean_ci(
-    data, limits=(0.2, 0.2), inclusive=(True, True), alpha: float = 0.05, axis: Untyped | None = None
+    data,
+    limits=(0.2, 0.2),
+    inclusive=(True, True),
+    alpha: float = 0.05,
+    axis: Untyped | None = None,
 ) -> Untyped: ...
 def mjci(data, prob=(0.25, 0.5, 0.75), axis: Untyped | None = None) -> Untyped: ...
 def mquantiles_cimj(data, prob=(0.25, 0.5, 0.75), alpha: float = 0.05, axis: Untyped | None = None) -> Untyped: ...
