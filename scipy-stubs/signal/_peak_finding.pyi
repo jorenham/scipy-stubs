88ea09c5a635d7d5d932b728aeda419f7e5c3868
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-10-16 18:15:11 +1100

    `signal`: Add type stubs for `find_peaks` and `find_peaks_cwt`

--- scipy-stubs/signal/_peak_finding.pyi
+++ scipy-stubs/signal/_peak_finding.pyi
@@ -1,9 +1,9 @@
-from collections.abc import Callable
-from typing import Literal, TypeAlias
+from collections.abc import Callable, Sequence
+from typing import Concatenate, Literal, TypeAlias, TypedDict
+from typing_extensions import NotRequired
 
 import numpy as np
 import numpy.typing as npt
-from scipy._typing import Untyped
 
 __all__ = ["argrelextrema", "argrelmax", "argrelmin", "find_peaks", "find_peaks_cwt", "peak_prominences", "peak_widths"]
 
@@ -13,10 +13,26 @@ _WidthsResult: TypeAlias = tuple[
     npt.NDArray[np.float64], npt.NDArray[np.float64], npt.NDArray[np.float64], npt.NDArray[np.float64]
 ]
 
+class _FindPeaksResultsDict(TypedDict):
+    peak_heights: NotRequired[npt.NDArray[np.float64]]
+    left_thresholds: NotRequired[npt.NDArray[np.float64]]
+    right_thresholds: NotRequired[npt.NDArray[np.float64]]
+    prominences: NotRequired[npt.NDArray[np.float64]]
+    left_bases: NotRequired[npt.NDArray[np.intp]]
+    right_bases: NotRequired[npt.NDArray[np.intp]]
+    width_heights: NotRequired[npt.NDArray[np.float64]]
+    left_ips: NotRequired[npt.NDArray[np.float64]]
+    right_ips: NotRequired[npt.NDArray[np.float64]]
+    plateau_sizes: NotRequired[npt.NDArray[np.intp]]
+    left_edges: NotRequired[npt.NDArray[np.intp]]
+    right_edges: NotRequired[npt.NDArray[np.intp]]
+
 def argrelmin(
     data: npt.NDArray[np.generic], axis: int = 0, order: int = 1, mode: _Mode = "clip"
 ) -> tuple[npt.NDArray[np.intp], ...]: ...
-def argrelmax(data: Untyped, axis: int = 0, order: int = 1, mode: _Mode = "clip") -> tuple[npt.NDArray[np.intp], ...]: ...
+def argrelmax(
+    data: npt.NDArray[np.generic], axis: int = 0, order: int = 1, mode: _Mode = "clip"
+) -> tuple[npt.NDArray[np.intp], ...]: ...
 def argrelextrema(
     data: npt.NDArray[np.generic],
     comparator: Callable[[npt.NDArray[np.generic], npt.NDArray[np.generic]], npt.NDArray[np.bool_]],
@@ -33,24 +49,24 @@ def peak_widths(
     wlen: int | None = None,
 ) -> _WidthsResult: ...
 def find_peaks(
-    x: Untyped,
-    height: Untyped | None = None,
-    threshold: Untyped | None = None,
-    distance: Untyped | None = None,
-    prominence: Untyped | None = None,
-    width: Untyped | None = None,
-    wlen: Untyped | None = None,
+    x: npt.ArrayLike,
+    height: float | npt.NDArray[np.float64] | tuple[float | None, float | None] | None = None,
+    threshold: float | npt.NDArray[np.float64] | tuple[float | None, float | None] | None = None,
+    distance: np.float64 | None = None,
+    prominence: float | npt.NDArray[np.float64] | tuple[float | None, float | None] | None = None,
+    width: float | npt.NDArray[np.float64] | tuple[float | None, float | None] | None = None,
+    wlen: int | None = None,
     rel_height: float = 0.5,
-    plateau_size: Untyped | None = None,
-) -> Untyped: ...
+    plateau_size: float | npt.NDArray[np.intp] | tuple[int | None, int | None] | None = None,
+) -> tuple[npt.NDArray[np.intp], _FindPeaksResultsDict]: ...
 def find_peaks_cwt(
-    vector: Untyped,
-    widths: Untyped,
-    wavelet: Untyped | None = None,
-    max_distances: Untyped | None = None,
-    gap_thresh: Untyped | None = None,
-    min_length: Untyped | None = None,
+    vector: npt.ArrayLike,
+    widths: npt.ArrayLike,
+    wavelet: Callable[Concatenate[int, float, ...], npt.NDArray[np.float64]] | None = None,
+    max_distances: Sequence[int] | None = None,
+    gap_thresh: float | None = None,
+    min_length: int | None = None,
     min_snr: int = 1,
     noise_perc: int = 10,
-    window_size: Untyped | None = None,
-) -> Untyped: ...
+    window_size: int | None = None,
+) -> npt.NDArray[np.intp]: ...
