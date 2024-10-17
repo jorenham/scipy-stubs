77c60174bd23ecc621518e1ca435a40f6147b497
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-10-17 13:49:52 +1100

    `signal`: Use `scipy._typing`'s `AnyReal and `AnyInt`
    In `_peak_finding.pyi` use them instead of `float` and `int`
    respectively. This is to allow usage of numpy scalars like `float16` and
    `int32`.

--- scipy-stubs/signal/_peak_finding.pyi
+++ scipy-stubs/signal/_peak_finding.pyi
@@ -5,6 +5,7 @@ import numpy as np
 import numpy.typing as npt
 import optype as op
 from numpy._typing import _ArrayLikeInt_co
+from scipy._typing import AnyInt, AnyReal
 
 __all__ = ["argrelextrema", "argrelmax", "argrelmin", "find_peaks", "find_peaks_cwt", "peak_prominences", "peak_widths"]
 
@@ -51,34 +52,34 @@ def argrelextrema(
 def peak_prominences(
     x: npt.ArrayLike,
     peaks: _ArrayLikeInt_co,
-    wlen: int | None = None,
+    wlen: AnyReal | None = None,
 ) -> _ProminencesResult: ...
 def peak_widths(
     x: npt.ArrayLike,
     peaks: _ArrayLikeInt_co,
-    rel_height: float = 0.5,
+    rel_height: AnyReal = 0.5,
     prominence_data: _ProminencesResult | None = None,
-    wlen: int | None = None,
+    wlen: AnyReal | None = None,
 ) -> _WidthsResult: ...
 def find_peaks(
     x: npt.ArrayLike,
-    height: float | _Array_f8 | tuple[float | None, float | None] | None = None,
-    threshold: float | _Array_f8 | tuple[float | None, float | None] | None = None,
-    distance: np.float64 | None = None,
-    prominence: float | _Array_f8 | tuple[float | None, float | None] | None = None,
-    width: float | _Array_f8 | tuple[float | None, float | None] | None = None,
-    wlen: int | None = None,
-    rel_height: float = 0.5,
-    plateau_size: int | _Array_n | tuple[int | None, int | None] | None = None,
+    height: AnyReal | _Array_f8 | tuple[AnyReal | None, AnyReal | None] | None = None,
+    threshold: AnyReal | _Array_f8 | tuple[AnyReal | None, AnyReal | None] | None = None,
+    distance: AnyReal | None = None,
+    prominence: AnyReal | _Array_f8 | tuple[AnyReal | None, AnyReal | None] | None = None,
+    width: AnyReal | _Array_f8 | tuple[AnyReal | None, AnyReal | None] | None = None,
+    wlen: AnyReal | None = None,
+    rel_height: AnyReal = 0.5,
+    plateau_size: AnyInt | _Array_n | tuple[AnyInt | None, AnyInt | None] | None = None,
 ) -> tuple[_Array_n, _FindPeaksResultsDict]: ...
 def find_peaks_cwt(
     vector: npt.ArrayLike,
     widths: npt.ArrayLike,
     wavelet: Callable[Concatenate[int, float, ...], _Array_f8] | None = None,
     max_distances: Sequence[int] | None = None,
-    gap_thresh: float | None = None,
-    min_length: int | None = None,
-    min_snr: int = 1,
-    noise_perc: int = 10,
-    window_size: int | None = None,
+    gap_thresh: AnyInt | None = None,
+    min_length: AnyInt | None = None,
+    min_snr: AnyReal = 1,
+    noise_perc: AnyReal = 10,
+    window_size: AnyInt | None = None,
 ) -> npt.NDArray[np.intp]: ...
