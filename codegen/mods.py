c41fde0e95185b93ce1f8fcbe5818e333b9dc951
jorenham <jhammudoglu@gmail.com>
2024-10-17 22:25:36 +0200

    move `codemods.py` to the new  `codegen/mods.py` package

--- codegen/mods.py
+++ codegen/mods.py
@@ -8,12 +8,18 @@
 from typing_extensions import override
 
 import libcst as cst
-from libcst.codemod import VisitorBasedCodemodCommand
+from libcst.codemod import CodemodContext, SkipFile, VisitorBasedCodemodCommand
 from libcst.codemod.visitors import AddImportsVisitor
 
 
 class AnnotateMissing(VisitorBasedCodemodCommand):
     DESCRIPTION = "Sets the default return type to `None`, and other missing annotations to `scipy._typing.Untyped`"
+
+    updated: int
+
+    def __init__(self, /, context: CodemodContext) -> None:
+        self.updated = 0
+        super().__init__(context)
 
     @override
     def leave_Param(self, /, original_node: cst.Param, updated_node: cst.Param) -> cst.Param:
@@ -21,6 +27,7 @@ class AnnotateMissing(VisitorBasedCodemodCommand):
             return updated_node
 
         AddImportsVisitor.add_needed_import(self.context, "scipy._typing", "Untyped")
+        self.updated += 1
         return updated_node.with_changes(annotation=cst.Annotation(cst.Name("Untyped")))
 
     @override
@@ -28,4 +35,12 @@ class AnnotateMissing(VisitorBasedCodemodCommand):
         if updated_node.returns is not None:
             return updated_node
 
+        self.updated += 1
         return updated_node.with_changes(returns=cst.Annotation(cst.Name("None")))
+
+    @override
+    def leave_Module(self, original_node: cst.Module, updated_node: cst.Module) -> cst.Module:
+        if not self.updated:
+            raise SkipFile("unchanged")
+
+        return updated_node
