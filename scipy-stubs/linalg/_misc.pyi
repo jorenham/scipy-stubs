d5b3910095ce276893d8850bb2231bec4556ef39
jorenham <jhammudoglu@gmail.com>
2024-10-18 17:12:29 +0200

    accept broader boolean arguments in `scipy.linalg.norm`

--- scipy-stubs/linalg/_misc.pyi
+++ scipy-stubs/linalg/_misc.pyi
@@ -4,6 +4,7 @@ import numpy as np
 import numpy.typing as npt
 import optype.typing as opt
 from numpy.linalg import LinAlgError  # noqa: ICN003
+from scipy._typing import AnyBool
 
 __all__ = ["LinAlgError", "LinAlgWarning", "norm"]
 
@@ -14,16 +15,16 @@ def norm(
     a: npt.ArrayLike,
     ord: Literal["fro", "nuc", 0, 1, -1, 2, -2] | float | None = None,
     axis: None = None,
-    keepdims: bool = False,
-    check_finite: bool = True,
+    keepdims: AnyBool = False,
+    check_finite: AnyBool = True,
 ) -> np.float64: ...
 @overload
 def norm(
     a: npt.ArrayLike,
     ord: Literal["fro", "nuc", 0, 1, -1, 2, -2] | float | None,
     axis: opt.AnyInt | tuple[opt.AnyInt, ...],
-    keepdims: bool = False,
-    check_finite: bool = True,
+    keepdims: AnyBool = False,
+    check_finite: AnyBool = True,
 ) -> np.float64 | npt.NDArray[np.float64]: ...
 @overload
 def norm(
@@ -31,6 +32,6 @@ def norm(
     ord: Literal["fro", "nuc", 0, 1, -1, 2, -2] | float | None = None,
     *,
     axis: opt.AnyInt | tuple[opt.AnyInt, ...],
-    keepdims: bool = False,
-    check_finite: bool = True,
+    keepdims: AnyBool = False,
+    check_finite: AnyBool = True,
 ) -> np.float64 | npt.NDArray[np.float64]: ...
