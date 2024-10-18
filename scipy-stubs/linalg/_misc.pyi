c3251f4b1e604b24dc412c469da88c42c46f0570
jorenham <jhammudoglu@gmail.com>
2024-10-18 17:06:07 +0200

    complete `scipy.linalg.norm`

--- scipy-stubs/linalg/_misc.pyi
+++ scipy-stubs/linalg/_misc.pyi
@@ -1,18 +1,36 @@
-from typing import Literal
+from typing import Literal, overload
 
 import numpy as np
 import numpy.typing as npt
+import optype.typing as opt
 from numpy.linalg import LinAlgError  # noqa: ICN003
-from scipy._typing import Untyped
 
 __all__ = ["LinAlgError", "LinAlgWarning", "norm"]
 
 class LinAlgWarning(RuntimeWarning): ...
 
+@overload
 def norm(
     a: npt.ArrayLike,
     ord: Literal["fro", "nuc", 0, 1, -1, 2, -2] | float | None = None,
-    axis: Untyped | None = None,
+    axis: None = None,
+    keepdims: bool = False,
+    check_finite: bool = True,
+) -> np.float64: ...
+@overload
+def norm(
+    a: npt.ArrayLike,
+    ord: Literal["fro", "nuc", 0, 1, -1, 2, -2] | float | None,
+    axis: opt.AnyInt | tuple[opt.AnyInt, ...],
+    keepdims: bool = False,
+    check_finite: bool = True,
+) -> np.float64 | npt.NDArray[np.float64]: ...
+@overload
+def norm(
+    a: npt.ArrayLike,
+    ord: Literal["fro", "nuc", 0, 1, -1, 2, -2] | float | None = None,
+    *,
+    axis: opt.AnyInt | tuple[opt.AnyInt, ...],
     keepdims: bool = False,
     check_finite: bool = True,
 ) -> np.float64 | npt.NDArray[np.float64]: ...
