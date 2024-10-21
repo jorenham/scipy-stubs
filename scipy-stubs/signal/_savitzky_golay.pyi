d25d4752611c74faa18c5e9e29bc08938f967070
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-10-21 12:50:23 +1100

    `signal`: Add types to `savgol_filter` for untyped parameters
    For the file `_savitzy_golay.pyi`.
    
    Additionally write a mode type alias for mode to specify the actually
    allowed strings.

--- scipy-stubs/signal/_savitzky_golay.pyi
+++ scipy-stubs/signal/_savitzky_golay.pyi
@@ -2,9 +2,9 @@ from typing import Literal, TypeAlias
 
 import numpy as np
 import numpy.typing as npt
-from scipy._typing import Untyped, UntypedArray
 
 _Array_fc_1d: TypeAlias = np.ndarray[tuple[int], np.dtype[np.inexact[npt.NBitBase]]]
+_Mode: TypeAlias = Literal["mirror", "constant", "nearest", "wrap", "interp"]
 
 def savgol_coeffs(
     window_length: int,
@@ -15,12 +15,12 @@ def savgol_coeffs(
     use: Literal["conv", "dot"] = "conv",
 ) -> _Array_fc_1d: ...
 def savgol_filter(
-    x: Untyped,
+    x: npt.ArrayLike,
     window_length: int,
     polyorder: int,
     deriv: int = 0,
     delta: float = 1.0,
     axis: int = -1,
-    mode: str = "interp",
+    mode: _Mode = "interp",
     cval: float = 0.0,
-) -> UntypedArray: ...
+) -> npt.NDArray[np.float32 | np.float64]: ...
