cce5aa7eb08949503445afc762ba5941aa820cd6
jorenham <jhammudoglu@gmail.com>
2024-10-22 23:48:44 +0200

    complete `scipy.stats._tukeylambda_stats`

--- scipy-stubs/stats/_tukeylambda_stats.pyi
+++ scipy-stubs/stats/_tukeylambda_stats.pyi
@@ -1,5 +1,25 @@
-from scipy._typing import Untyped
-from scipy.special import beta as beta
+from typing import Any, TypeAlias, TypeVar, overload
 
-def tukeylambda_variance(lam: Untyped) -> Untyped: ...
-def tukeylambda_kurtosis(lam: Untyped) -> Untyped: ...
+import numpy as np
+import numpy.typing as npt
+import optype.numpy as onpt
+from numpy._typing import _ArrayLikeFloat_co
+from scipy._typing import AnyReal
+
+_ShapeT = TypeVar("_ShapeT", bound=tuple[int, ...])
+_DTypeLikeFloat_co: TypeAlias = np.dtype[np.floating[Any] | np.integer[Any] | np.bool_]
+
+@overload
+def tukeylambda_variance(lam: AnyReal) -> onpt.Array[tuple[()], np.float64]: ...
+@overload
+def tukeylambda_variance(lam: onpt.CanArray[_ShapeT, _DTypeLikeFloat_co]) -> onpt.Array[_ShapeT, np.float64]: ...
+@overload
+def tukeylambda_variance(lam: _ArrayLikeFloat_co) -> npt.NDArray[np.float64]: ...
+
+#
+@overload
+def tukeylambda_kurtosis(lam: AnyReal) -> onpt.Array[tuple[()], np.float64]: ...
+@overload
+def tukeylambda_kurtosis(lam: onpt.CanArray[_ShapeT, _DTypeLikeFloat_co]) -> onpt.Array[_ShapeT, np.float64]: ...
+@overload
+def tukeylambda_kurtosis(lam: _ArrayLikeFloat_co) -> npt.NDArray[np.float64]: ...
