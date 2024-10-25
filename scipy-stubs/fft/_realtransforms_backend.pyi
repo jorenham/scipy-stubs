f8584315b756f8d676ea2118970cb14777da4998
jorenham <jhammudoglu@gmail.com>
2024-10-25 05:56:16 +0200

    complete `scipy.fft._realtransforms_backend`

--- scipy-stubs/fft/_realtransforms_backend.pyi
+++ scipy-stubs/fft/_realtransforms_backend.pyi
@@ -1,33 +1,28 @@
-from scipy._typing import DCTType, NormalizationMode, Untyped
-from ._realtransforms import (
-    dct as dct,
-    dctn as dctn,
-    dst as dst,
-    dstn as dstn,
-    idct as idct,
-    idst as idst,
-)
+from numpy._typing import _ArrayLikeInt, _ArrayLikeNumber_co
+from optype import CanBool
+from scipy._typing import AnyInt, AnyShape, DCTType, NormalizationMode
+from ._realtransforms import _ArrayReal, dct as dct, dctn as dctn, dst as dst, dstn as dstn, idct as idct, idst as idst
 
 # NOTE: Unlike the ones in `scipy.fft._realtransforms`, `orthogonalize` is keyword-only here.
 def idctn(
-    x: Untyped,
+    x: _ArrayLikeNumber_co,
     type: DCTType = 2,
-    s: Untyped | None = None,
-    axes: Untyped | None = None,
+    s: _ArrayLikeInt | None = None,
+    axes: AnyShape | None = None,
     norm: NormalizationMode | None = None,
-    overwrite_x: bool = False,
-    workers: int | None = None,
+    overwrite_x: CanBool = False,
+    workers: AnyInt | None = None,
     *,
-    orthogonalize: bool | None = None,
-) -> Untyped: ...
+    orthogonalize: CanBool | None = None,
+) -> _ArrayReal: ...
 def idstn(
-    x: Untyped,
+    x: _ArrayLikeNumber_co,
     type: DCTType = 2,
-    s: Untyped | None = None,
-    axes: Untyped | None = None,
+    s: _ArrayLikeInt | None = None,
+    axes: AnyShape | None = None,
     norm: NormalizationMode | None = None,
-    overwrite_x: bool = False,
-    workers: int | None = None,
+    overwrite_x: CanBool = False,
+    workers: AnyInt | None = None,
     *,
-    orthogonalize: bool | None = None,
-) -> Untyped: ...
+    orthogonalize: CanBool | None = None,
+) -> _ArrayReal: ...
