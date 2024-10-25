c9e25ada6cb105482b27a07b9342a46c749b54e7
jorenham <jhammudoglu@gmail.com>
2024-10-25 05:19:18 +0200

    re-use  `scipy.fft` function signatures in the `*_backend` modules

--- scipy-stubs/fft/_realtransforms_backend.pyi
+++ scipy-stubs/fft/_realtransforms_backend.pyi
@@ -1,86 +1,33 @@
-from scipy._lib._array_api import array_namespace as array_namespace
-from scipy._typing import Untyped
+from scipy._typing import DCTType, NormalizationMode, Untyped
+from ._realtransforms import (
+    dct as dct,
+    dctn as dctn,
+    dst as dst,
+    dstn as dstn,
+    idct as idct,
+    idst as idst,
+)
 
-def dctn(
-    x: Untyped,
-    type: int = 2,
-    s: Untyped | None = None,
-    axes: Untyped | None = None,
-    norm: Untyped | None = None,
-    overwrite_x: bool = False,
-    workers: Untyped | None = None,
-    *,
-    orthogonalize: Untyped | None = None,
-) -> Untyped: ...
+# NOTE: Unlike the ones in `scipy.fft._realtransforms`, `orthogonalize` is keyword-only here.
 def idctn(
     x: Untyped,
-    type: int = 2,
+    type: DCTType = 2,
     s: Untyped | None = None,
     axes: Untyped | None = None,
-    norm: Untyped | None = None,
+    norm: NormalizationMode | None = None,
     overwrite_x: bool = False,
-    workers: Untyped | None = None,
+    workers: int | None = None,
     *,
-    orthogonalize: Untyped | None = None,
-) -> Untyped: ...
-def dstn(
-    x: Untyped,
-    type: int = 2,
-    s: Untyped | None = None,
-    axes: Untyped | None = None,
-    norm: Untyped | None = None,
-    overwrite_x: bool = False,
-    workers: Untyped | None = None,
-    orthogonalize: Untyped | None = None,
+    orthogonalize: bool | None = None,
 ) -> Untyped: ...
 def idstn(
     x: Untyped,
-    type: int = 2,
+    type: DCTType = 2,
     s: Untyped | None = None,
     axes: Untyped | None = None,
-    norm: Untyped | None = None,
+    norm: NormalizationMode | None = None,
     overwrite_x: bool = False,
-    workers: Untyped | None = None,
+    workers: int | None = None,
     *,
-    orthogonalize: Untyped | None = None,
-) -> Untyped: ...
-def dct(
-    x: Untyped,
-    type: int = 2,
-    n: Untyped | None = None,
-    axis: int = -1,
-    norm: Untyped | None = None,
-    overwrite_x: bool = False,
-    workers: Untyped | None = None,
-    orthogonalize: Untyped | None = None,
-) -> Untyped: ...
-def idct(
-    x: Untyped,
-    type: int = 2,
-    n: Untyped | None = None,
-    axis: int = -1,
-    norm: Untyped | None = None,
-    overwrite_x: bool = False,
-    workers: Untyped | None = None,
-    orthogonalize: Untyped | None = None,
-) -> Untyped: ...
-def dst(
-    x: Untyped,
-    type: int = 2,
-    n: Untyped | None = None,
-    axis: int = -1,
-    norm: Untyped | None = None,
-    overwrite_x: bool = False,
-    workers: Untyped | None = None,
-    orthogonalize: Untyped | None = None,
-) -> Untyped: ...
-def idst(
-    x: Untyped,
-    type: int = 2,
-    n: Untyped | None = None,
-    axis: int = -1,
-    norm: Untyped | None = None,
-    overwrite_x: bool = False,
-    workers: Untyped | None = None,
-    orthogonalize: Untyped | None = None,
+    orthogonalize: bool | None = None,
 ) -> Untyped: ...
