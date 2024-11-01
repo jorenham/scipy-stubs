a6837bd2fa10f28df3ed33bfa144009dc66881fc
jorenham <jhammudoglu@gmail.com>
2024-11-01 03:50:03 +0100

    improved (type-)test structure

--- tests/integrate/test_quad.pyi
+++ tests/integrate/test_quad.pyi
@@ -10,31 +10,31 @@ from scipy.integrate._typing import QuadInfoDict
 TRUE: Literal[True] = True
 
 # ufunc
-_ = assert_type(quad(np.exp, 0, 1), tuple[float, float])
+assert_type(quad(np.exp, 0, 1), tuple[float, float])
 
 # (float) -> float
 f0_float_float: Callable[[float], float]
-_ = assert_type(quad(f0_float_float, 0, 1), tuple[float, float])
+assert_type(quad(f0_float_float, 0, 1), tuple[float, float])
 
 # (float) -> np.float64
 f0_float_f8: Callable[[float], np.float64]
-_ = assert_type(quad(f0_float_f8, 0, 1), tuple[float, float])
+assert_type(quad(f0_float_f8, 0, 1), tuple[float, float])
 
 # (np.float64) -> float
 f0_f8_float: Callable[[np.float64], float]
-_ = assert_type(quad(f0_f8_float, 0, 1), tuple[float, float])
+assert_type(quad(f0_f8_float, 0, 1), tuple[float, float])
 
 # (float, str) -> float
 f1_float_float: Callable[[float, str], float]
-_ = assert_type(quad(f1_float_float, 0, 1, args=("",)), tuple[float, float])
+assert_type(quad(f1_float_float, 0, 1, args=("",)), tuple[float, float])
 
 # (float, str, str) -> float
 f2_float_float: Callable[[float, str, str], float]
-_ = assert_type(quad(f2_float_float, 0, 1, args=("", "")), tuple[float, float])
+assert_type(quad(f2_float_float, 0, 1, args=("", "")), tuple[float, float])
 
 # (float) -> float, full output
 # NOTE: this test fails (only) in mypy due to some mypy bug
-_ = assert_type(  # type: ignore[assignment]
+assert_type(
     quad(f0_float_float, 0, 1, full_output=TRUE),
     tuple[float, float, QuadInfoDict]
     | tuple[float, float, QuadInfoDict, str]
@@ -44,4 +44,4 @@ _ = assert_type(  # type: ignore[assignment]
 # (float) -> complex
 # NOTE: this test fails (only) in mypy due to some mypy bug
 z0_float_complex: Callable[[float], complex]
-_ = assert_type(quad(z0_float_complex, 0, 1, complex_func=TRUE), tuple[complex, complex])  # type: ignore[assignment]
+assert_type(quad(z0_float_complex, 0, 1, complex_func=TRUE), tuple[complex, complex])
---
+++ tests/stats/test_rv_frozen.pyi
@@ -0,0 +1,25 @@
+from typing import TypeAlias
+from typing_extensions import assert_type
+
+import numpy as np
+from scipy.stats import distributions as d
+
+_Value_f8: TypeAlias = float | np.float64
+_Array_f8: TypeAlias = _Value_f8 | np.ndarray[tuple[int, ...], np.dtype[np.float64]]
+
+# test `rv_continuous_frozen`
+assert_type(d.uniform().mean(), _Value_f8)
+assert_type(d.uniform(0).mean(), _Value_f8)
+assert_type(d.uniform(0.5, 2).mean(), _Value_f8)
+assert_type(d.uniform([0, -1]).mean(), _Array_f8)
+assert_type(d.uniform([0, 0.5], 2).mean(), _Array_f8)
+assert_type(d.uniform(0, [0.5, 2]).mean(), _Array_f8)
+
+# test `rv_discrete_frozen`
+assert_type(d.bernoulli().mean(), _Value_f8)
+assert_type(d.bernoulli(0).mean(), _Value_f8)
+assert_type(d.bernoulli(0.5).mean(), _Value_f8)
+assert_type(d.bernoulli([0, -1]).mean(), _Array_f8)
+assert_type(d.bernoulli([0, 0.5]).mean(), _Array_f8)
+
+# TODO: more tests
