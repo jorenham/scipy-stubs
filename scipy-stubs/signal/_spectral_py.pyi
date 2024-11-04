f7ba43912000922d74106418f19f10a7e905fd1a
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-11-04 11:43:24 +1100

    `signal`: Add type stubs for function `csd`.
    In `_spectral_py.pyi`.

--- scipy-stubs/signal/_spectral_py.pyi
+++ scipy-stubs/signal/_spectral_py.pyi
@@ -1,14 +1,20 @@
-from typing import Literal, TypeAlias
+from collections.abc import Callable
+from typing import Any, Literal, TypeAlias
 from typing_extensions import Unpack
 
+import numpy as np
+import numpy.typing as npt
 import optype as op
-from numpy._typing import _ArrayLikeFloat_co
+from numpy._typing import _ArrayLikeComplex_co, _ArrayLikeFloat_co
 from scipy._typing import AnyInt, AnyReal, Untyped, UntypedCallable
 from scipy.signal.windows._windows import _Window, _WindowNeedsParams
 
 __all__ = ["check_COLA", "check_NOLA", "coherence", "csd", "istft", "lombscargle", "periodogram", "spectrogram", "stft", "welch"]
 
 _GetWindowArgument: TypeAlias = _Window | tuple[_Window | _WindowNeedsParams, Unpack[tuple[object, ...]]]
+_Detrend: TypeAlias = Literal["literal", "constant", False] | Callable[[npt.NDArray[np.generic]], npt.NDArray[np.generic]]
+_Scaling: TypeAlias = Literal["density", "spectrum"]
+_Average: TypeAlias = Literal["mean", "median"]
 
 def lombscargle(x: Untyped, y: Untyped, freqs: Untyped, precenter: bool = False, normalize: bool = False) -> Untyped: ...
 def periodogram(
@@ -35,19 +41,19 @@ def welch(
     average: str = "mean",
 ) -> Untyped: ...
 def csd(
-    x: Untyped,
-    y: Untyped,
-    fs: float = 1.0,
-    window: str = "hann",
-    nperseg: int | None = None,
-    noverlap: int | None = None,
-    nfft: int | None = None,
-    detrend: str | Literal[False] | UntypedCallable = "constant",
-    return_onesided: bool = True,
-    scaling: str = "density",
-    axis: int = -1,
-    average: str = "mean",
-) -> Untyped: ...
+    x: _ArrayLikeComplex_co,
+    y: _ArrayLikeComplex_co,
+    fs: AnyReal = 1.0,
+    window: _GetWindowArgument | _ArrayLikeFloat_co = "hann",
+    nperseg: AnyInt | None = None,
+    noverlap: AnyInt | None = None,
+    nfft: AnyInt | None = None,
+    detrend: _Detrend = "constant",
+    return_onesided: op.CanBool = True,
+    scaling: _Scaling = "density",
+    axis: op.CanIndex = -1,
+    average: _Average = "mean",
+) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.complexfloating[Any, Any]]]: ...
 def spectrogram(
     x: Untyped,
     fs: float = 1.0,
