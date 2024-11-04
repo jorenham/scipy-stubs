973820e6368bb10b01c01eabfa225d0b03446ae5
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-11-04 13:02:10 +1100

    tests: Add type tests for function overloads of `istft`.
    From the module `signal`.

--- tests/signal/test_spectral.pyi
+++ tests/signal/test_spectral.pyi
@@ -3,7 +3,7 @@ from typing_extensions import assert_type
 
 import numpy as np
 import numpy.typing as npt
-from scipy.signal import spectrogram
+from scipy.signal import istft, spectrogram
 
 _Array_f8: TypeAlias = npt.NDArray[np.float64]
 _ArrayReal: TypeAlias = npt.NDArray[np.floating[Any]]
@@ -19,4 +19,47 @@ assert_type(spectrogram(np.linspace(200, 300, 256), mode="complex"), tuple[_Arra
 assert_type(
     spectrogram(np.linspace(200, 300, 256), 1.0, ("tukey", 2.5), None, None, None, "constant", True, "density", -1, "complex"),
     tuple[_Array_f8, _Array_f8, _ArrayComplex],
+)
+
+# test isft function overloads
+assert_type(istft(np.ones((129, 100), dtype=np.complex128)), tuple[_Array_f8, _ArrayReal])
+assert_type(istft(np.ones((129, 100), dtype=np.complex128), input_onesided=True), tuple[_Array_f8, _ArrayReal])
+assert_type(istft(np.ones((129, 100), dtype=np.complex128), 1.0, "hann", 256, 128, 256, False), tuple[_Array_f8, _ArrayComplex])
+assert_type(
+    istft(
+        np.ones((129, 100), dtype=np.complex128), input_onesided=False, fs=1.0, window="hann", nperseg=256, noverlap=128, nfft=256
+    ),
+    tuple[_Array_f8, _ArrayComplex],
+)
+assert_type(
+    istft(
+        np.ones((129, 100), dtype=np.complex128),
+        fs=2.0,
+        window=("tukey", 0.25),
+        nperseg=256,
+        noverlap=128,
+        nfft=256,
+        input_onesided=True,
+        boundary=False,
+        time_axis=-1,
+        freq_axis=0,
+        scaling="spectrum",
+    ),
+    tuple[_Array_f8, _ArrayReal],
+)
+assert_type(
+    istft(
+        np.ones((129, 100), dtype=np.complex128),
+        fs=2.0,
+        window=("tukey", 0.25),
+        nperseg=256,
+        noverlap=128,
+        nfft=256,
+        input_onesided=False,
+        boundary=False,
+        time_axis=0,
+        freq_axis=1,
+        scaling="spectrum",
+    ),
+    tuple[_Array_f8, _ArrayComplex],
 )
