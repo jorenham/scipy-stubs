3bc1a67c7b679188be2fee6b3dbf62e71e611205
jorenham <jhammudoglu@gmail.com>
2024-11-04 23:16:04 +0100

    fix and improve `scipy.spatial._[c]kdtree`

--- scipy-stubs/spatial/_ckdtree.pyi
+++ scipy-stubs/spatial/_ckdtree.pyi
@@ -1,12 +1,9 @@
-# NOTE(jorenham): There are alteady inline stubs for this module, so this won't be used (which is unfortunate, because the inline
-# annotations are wrong in several places, although I've seen worse).
-
-from typing import Literal, TypeAlias, overload, type_check_only
+from typing import Literal as L, TypeAlias, overload, type_check_only
 
 import numpy as np
 import numpy.typing as npt
 from numpy._typing import _ArrayLikeFloat_co, _ArrayLikeInt
-from scipy._typing import AnyScalar
+from scipy._typing import AnyReal, AnyScalar
 from scipy.sparse import coo_matrix, dok_matrix
 
 __all__ = ["cKDTree"]
@@ -77,81 +74,93 @@ class cKDTree(_CythonMixin):
         balanced_tree: bool = ...,
         boxsize: npt.ArrayLike | None = ...,
     ) -> None: ...
+
+    #
     def query(
         self,
+        /,
         x: _ArrayLikeFloat_co,
         k: _ArrayLikeInt = 1,
-        eps: float = 0.0,
-        p: float = 2.0,
+        eps: AnyReal = 0.0,
+        p: AnyReal = 2.0,
         distance_upper_bound: float = ...,  # inf
         workers: int | None = None,
-    ) -> tuple[float, float] | tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]: ...
+    ) -> tuple[float, np.intp] | tuple[npt.NDArray[np.float64], npt.NDArray[np.intp]]: ...
+
+    #
     def query_ball_point(
         self,
+        /,
         x: _ArrayLikeFloat_co,
         r: npt.ArrayLike,
-        p: float = 2.0,
-        eps: float = 0.0,
+        p: AnyReal = 2.0,
+        eps: AnyReal = 0.0,
         workers: int | None = None,
         return_sorted: bool | None = None,
         return_length: bool = False,
     ) -> list[int] | npt.NDArray[np.object_]: ...
-    def query_ball_tree(
-        self,
-        other: cKDTree,
-        r: float,
-        p: float = 2.0,
-        eps: float = 0.0,
-    ) -> list[list[int]]: ...
+
+    #
+    def query_ball_tree(self, /, other: cKDTree, r: AnyReal, p: AnyReal = 2.0, eps: AnyReal = 0.0) -> list[list[int]]: ...
+
+    #
     @overload
     def query_pairs(
         self,
-        r: float,
-        p: float = 2.0,
-        eps: float = 0,
-        output_type: Literal["set"] = "set",
+        /,
+        r: AnyReal,
+        p: AnyReal = 2.0,
+        eps: AnyReal = 0,
+        output_type: L["set"] = "set",
     ) -> set[tuple[int, int]]: ...
     @overload
     def query_pairs(
         self,
-        r: float,
-        p: float = 2.0,
-        eps: float = 0,
-        *,
-        output_type: Literal["ndarray"],
+        /,
+        r: AnyReal,
+        p: AnyReal,
+        eps: AnyReal,
+        output_type: L["ndarray"],
     ) -> npt.NDArray[np.intp]: ...
     @overload
     def query_pairs(
         self,
-        r: float,
-        p: float,
-        eps: float,
-        output_type: Literal["ndarray"],
+        /,
+        r: AnyReal,
+        p: AnyReal = 2.0,
+        eps: AnyReal = 0,
+        *,
+        output_type: L["ndarray"],
     ) -> npt.NDArray[np.intp]: ...
+
+    #
     @overload
     def count_neighbors(
         self,
+        /,
         other: cKDTree,
         r: AnyScalar,
-        p: float = 2.0,
+        p: AnyReal = 2.0,
         weights: tuple[None, None] | None = None,
         cumulative: bool = True,
     ) -> np.intp: ...
     @overload
     def count_neighbors(
         self,
+        /,
         other: cKDTree,
         r: AnyScalar,
-        p: float,
+        p: AnyReal,
         weights: _Weights,
         cumulative: bool = True,
     ) -> np.float64: ...
     @overload
     def count_neighbors(
         self,
+        /,
         other: cKDTree,
         r: AnyScalar,
-        p: float = 2.0,
+        p: AnyReal = 2.0,
         *,
         weights: _Weights,
         cumulative: bool = True,
@@ -159,63 +168,72 @@ class cKDTree(_CythonMixin):
     @overload
     def count_neighbors(
         self,
+        /,
         other: cKDTree,
         r: npt.ArrayLike,
-        p: float = 2.0,
+        p: AnyReal = 2.0,
         weights: None | tuple[None, None] = ...,
         cumulative: bool = True,
     ) -> np.float64 | np.intp | npt.NDArray[np.intp]: ...
     @overload
     def count_neighbors(
         self,
+        /,
         other: cKDTree,
         r: npt.ArrayLike,
-        p: float,
+        p: AnyReal,
         weights: _Weights,
         cumulative: bool = True,
     ) -> np.float64 | np.intp | npt.NDArray[np.float64]: ...
     @overload
     def count_neighbors(
         self,
+        /,
         other: cKDTree,
         r: npt.ArrayLike,
-        p: float = 2.0,
+        p: AnyReal = 2.0,
         *,
         weights: _Weights,
         cumulative: bool = True,
     ) -> np.float64 | np.intp | npt.NDArray[np.float64]: ...
+
+    #
     @overload
     def sparse_distance_matrix(
         self,
+        /,
         other: cKDTree,
-        max_distance: float,
-        p: float = 2.0,
-        output_type: Literal["dok_matrix"] = ...,
+        max_distance: AnyReal,
+        p: AnyReal = 2.0,
+        output_type: L["dok_matrix"] = ...,
     ) -> dok_matrix: ...
     @overload
     def sparse_distance_matrix(
         self,
+        /,
         other: cKDTree,
-        max_distance: float,
-        p: float = 2.0,
+        max_distance: AnyReal,
+        p: AnyReal = 2.0,
         *,
-        output_type: Literal["coo_matrix"],
+        output_type: L["coo_matrix"],
     ) -> coo_matrix: ...
     @overload
     def sparse_distance_matrix(
         self,
+        /,
         other: cKDTree,
-        max_distance: float,
-        p: float = 2.0,
+        max_distance: AnyReal,
+        p: AnyReal = 2.0,
         *,
-        output_type: Literal["dict"],
+        output_type: L["dict"],
     ) -> dict[tuple[int, int], float]: ...
     @overload
     def sparse_distance_matrix(
         self,
+        /,
         other: cKDTree,
-        max_distance: float,
-        p: float = 2.0,
+        max_distance: AnyReal,
+        p: AnyReal = 2.0,
         *,
-        output_type: Literal["ndarray"],
+        output_type: L["ndarray"],
     ) -> npt.NDArray[np.void]: ...
