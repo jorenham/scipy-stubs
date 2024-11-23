25be180f4874cd037e07c3e4d5c44288c9ca7530
jorenham <jhammudoglu@gmail.com>
2024-11-23 15:55:59 +0100

    [sparse.csgraph] prefer `optype.numpy` over `numpy._typing` for array-likes

--- scipy-stubs/sparse/csgraph/_tools.pyi
+++ scipy-stubs/sparse/csgraph/_tools.pyi
@@ -3,40 +3,38 @@ from typing_extensions import TypeVar
 
 import numpy as np
 import optype.numpy as onp
-from numpy._typing import _ArrayLikeFloat_co
 from scipy.sparse import csc_array, csc_matrix, csr_array, csr_matrix, lil_array, lil_matrix, sparray, spmatrix
 
 _ShapeT = TypeVar("_ShapeT", bound=tuple[int, ...], default=tuple[int, ...])
-_SCT = TypeVar("_SCT", bound=_Float_co, default=_Float_co)
+_SCT = TypeVar("_SCT", bound=np.floating[Any] | np.integer[Any], default=np.floating[Any] | np.integer[Any])
 
-_Float_co: TypeAlias = np.floating[Any] | np.integer[Any] | np.bool_
 _MaskedArray: TypeAlias = np.ma.MaskedArray[_ShapeT, np.dtype[_SCT]]
 _SparseGraph: TypeAlias = csr_matrix | csc_matrix | lil_matrix | csr_array | csc_array | lil_array
-_GraphLike: TypeAlias = sparray | spmatrix | _ArrayLikeFloat_co
+_GraphLike: TypeAlias = onp.ToFloat2D | sparray | spmatrix
 
 DTYPE: Final[type[np.float64]] = ...
 ITYPE: Final[type[np.int32]] = ...
 
-def csgraph_from_masked(graph: _MaskedArray[Any]) -> csr_matrix: ...
+def csgraph_from_masked(graph: _MaskedArray) -> csr_matrix: ...
 def csgraph_masked_from_dense(
-    graph: _ArrayLikeFloat_co,
+    graph: onp.ToFloat2D,
     null_value: float | None = 0,
     nan_null: bool = True,
     infinity_null: bool = True,
     copy: bool = True,
 ) -> _MaskedArray[tuple[int, int], np.floating[Any]]: ...
 def csgraph_from_dense(
-    graph: _ArrayLikeFloat_co,
+    graph: onp.ToFloat2D,
     null_value: float | None = 0,
     nan_null: bool = True,
     infinity_null: bool = True,
 ) -> csr_matrix: ...
 def csgraph_to_dense(csgraph: _SparseGraph, null_value: float | None = 0) -> onp.Array2D[np.floating[Any]]: ...
 def csgraph_to_masked(csgraph: _SparseGraph) -> _MaskedArray[tuple[int, int], np.floating[Any]]: ...
-def reconstruct_path(csgraph: _GraphLike, predecessors: _ArrayLikeFloat_co, directed: bool = True) -> csr_matrix: ...
+def reconstruct_path(csgraph: _GraphLike, predecessors: onp.ToFloatND, directed: bool = True) -> csr_matrix: ...
 def construct_dist_matrix(
     graph: _GraphLike,
-    predecessors: _ArrayLikeFloat_co,
+    predecessors: onp.ToFloatND,
     directed: bool = True,
     null_value: float = ...,
 ) -> onp.Array2D[np.floating[Any]]: ...
--- scipy-stubs/sparse/csgraph/_validation.pyi
+++ scipy-stubs/sparse/csgraph/_validation.pyi
@@ -1,19 +1,17 @@
 from typing import Any, Final, TypeAlias
 
 import numpy as np
-import numpy.typing as npt
 import optype.numpy as onp
-from numpy._typing import _ArrayLikeFloat_co
 from scipy.sparse import sparray, spmatrix
 
-_GraphLike: TypeAlias = sparray | spmatrix | _ArrayLikeFloat_co
+_GraphLike: TypeAlias = onp.ToFloat2D | sparray | spmatrix
 
 DTYPE: Final[type[np.float64]] = ...
 
 def validate_graph(
     csgraph: _GraphLike,
     directed: bool,
-    dtype: npt.DTypeLike = ...,
+    dtype: onp.AnyFloatingDType = ...,
     csr_output: bool = True,
     dense_output: bool = True,
     copy_if_dense: bool = False,
