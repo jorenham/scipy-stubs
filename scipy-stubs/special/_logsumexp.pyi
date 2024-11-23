af7aea107ebab83a8578c0329f08e4fb790674c1
jorenham <jhammudoglu@gmail.com>
2024-11-23 16:41:51 +0100

    [special] prefer `optype.numpy` over `numpy._typing` for array-likes

--- scipy-stubs/special/_logsumexp.pyi
+++ scipy-stubs/special/_logsumexp.pyi
@@ -1,21 +1,50 @@
+from typing import Literal, overload
+
 import numpy as np
-import numpy.typing as npt
-from numpy._typing import _ArrayLikeComplex_co, _ArrayLikeFloat_co
+import optype.numpy as onp
 
 __all__ = ["log_softmax", "logsumexp", "softmax"]
 
+# TODO: Support `return_sign=True`
+@overload
 def logsumexp(
-    a: _ArrayLikeComplex_co,
+    a: onp.ToFloat,
     axis: int | tuple[int, ...] | None = None,
-    b: _ArrayLikeFloat_co | None = None,
+    b: onp.ToFloat | None = None,
     keepdims: bool = False,
-    return_sign: bool = False,
-) -> np.float64 | np.complex128 | npt.NDArray[np.float64 | np.complex128]: ...
+    return_sign: Literal[False, 0] = False,
+) -> np.float64: ...
+@overload
+def logsumexp(
+    a: onp.ToComplex,
+    axis: int | tuple[int, ...] | None = None,
+    b: onp.ToFloat | None = None,
+    keepdims: bool = False,
+    return_sign: Literal[False, 0] = False,
+) -> np.float64 | np.complex128: ...
+@overload
+def logsumexp(
+    a: onp.ToFloatND,
+    axis: int | tuple[int, ...],
+    b: onp.ToFloat | onp.ToFloatND | None = None,
+    keepdims: bool = False,
+    return_sign: Literal[False, 0] = False,
+) -> np.float64 | onp.ArrayND[np.float64]: ...
+@overload
+def logsumexp(
+    a: onp.ToComplexND,
+    axis: int | tuple[int, ...],
+    b: onp.ToFloat | onp.ToFloatND | None = None,
+    keepdims: bool = False,
+    return_sign: Literal[False, 0] = False,
+) -> np.float64 | np.complex128 | onp.ArrayND[np.float64 | np.complex128]: ...
+
+# TODO: Overload real/complex and scalar/array
 def softmax(
-    x: _ArrayLikeComplex_co,
+    x: onp.ToComplex | onp.ToComplexND,
     axis: int | tuple[int, ...] | None = None,
-) -> np.float64 | np.complex128 | npt.NDArray[np.float64 | np.complex128]: ...
+) -> np.float64 | np.complex128 | onp.ArrayND[np.float64 | np.complex128]: ...
 def log_softmax(
-    x: _ArrayLikeComplex_co,
+    x: onp.ToComplex | onp.ToComplexND,
     axis: int | tuple[int, ...] | None = None,
-) -> np.float64 | np.complex128 | npt.NDArray[np.float64 | np.complex128]: ...
+) -> np.float64 | np.complex128 | onp.ArrayND[np.float64 | np.complex128]: ...
