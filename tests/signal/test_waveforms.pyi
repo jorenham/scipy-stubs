d656dd63f71829864bc763a2d4b4286ff071a5f8
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-11-24 13:50:15 +1100

    tests: Update `test_waveforms`

--- tests/signal/test_waveforms.pyi
+++ tests/signal/test_waveforms.pyi
@@ -2,16 +2,14 @@ from typing import Literal, TypeAlias
 from typing_extensions import assert_type
 
 import numpy as np
-import numpy.typing as npt
 import optype.numpy as onp
 from scipy.signal import gausspulse
 
-_Array_f8: TypeAlias = npt.NDArray[np.float64]
+_Array_f8: TypeAlias = onp.ArrayND[np.float64]
 _Falsy: TypeAlias = Literal[0, False]
 _Truthy: TypeAlias = Literal[1, True]
 
-_time: onp.ToFloatND
-_int: onp.ToInt
+_time: onp.ToFloat | onp.ToFloatND
 _float: onp.ToFloat
 _truthy: _Truthy
 _falsy: _Falsy
@@ -19,17 +17,17 @@ _falsy: _Falsy
 # test gausspulse function overloads
 assert_type(gausspulse(_time), _Array_f8)
 # Full positional arguments
-assert_type(gausspulse(_time, _int, _float, _int, _int, _falsy, _falsy), _Array_f8)
+assert_type(gausspulse(_time, _float, _float, _float, _float, _falsy, _falsy), _Array_f8)
 assert_type(
-    gausspulse(_time, _int, _float, _int, _int, _truthy, _falsy),
+    gausspulse(_time, _float, _float, _float, _float, _truthy, _falsy),
     tuple[_Array_f8, _Array_f8],
 )
 assert_type(
-    gausspulse(_time, _int, _float, _int, _int, _falsy, _truthy),
+    gausspulse(_time, _float, _float, _float, _float, _falsy, _truthy),
     tuple[_Array_f8, _Array_f8],
 )
 assert_type(
-    gausspulse(_time, _int, _float, _int, _int, _truthy, _truthy),
+    gausspulse(_time, _float, _float, _float, _float, _truthy, _truthy),
     tuple[_Array_f8, _Array_f8, _Array_f8],
 )
 # Full keyword arguments
@@ -69,18 +67,18 @@ assert_type(
 
 # Mixed positional and keyword arguments
 assert_type(
-    gausspulse(_time, _int, _float, _int, _int, retquad=_falsy, retenv=_falsy),
+    gausspulse(_time, _float, _float, _float, _float, retquad=_falsy, retenv=_falsy),
     _Array_f8,
 )
 assert_type(
-    gausspulse(_time, _int, _float, _int, _int, retquad=_truthy, retenv=_falsy),
+    gausspulse(_time, _float, _float, _float, _float, retquad=_truthy, retenv=_falsy),
     tuple[_Array_f8, _Array_f8],
 )
 assert_type(
-    gausspulse(_time, _int, _float, _int, _int, retquad=_falsy, retenv=_truthy),
+    gausspulse(_time, _float, _float, _float, _float, retquad=_falsy, retenv=_truthy),
     tuple[_Array_f8, _Array_f8],
 )
 assert_type(
-    gausspulse(_time, _int, _float, _int, _int, retquad=_truthy, retenv=_truthy),
+    gausspulse(_time, _float, _float, _float, _float, retquad=_truthy, retenv=_truthy),
     tuple[_Array_f8, _Array_f8, _Array_f8],
 )
