3278292fb242e8b20e5a7dffe9fb11643c083cc7
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-11-24 14:22:41 +1100

    `signal`+tests: Add all possible overloads for `gausspulse`.
    `t` can be either an array or a scalar or "cutoff". Add corresponding
    tests

--- scipy-stubs/signal/_waveforms.pyi
+++ scipy-stubs/signal/_waveforms.pyi
@@ -16,7 +16,6 @@ _Truthy: TypeAlias = Literal[1, True]
 _Falsy: TypeAlias = Literal[0, False]
 _ArrayLikeFloat: TypeAlias = onp.ToFloat | onp.ToFloatND
 _Array_f8: TypeAlias = onp.ArrayND[np.float64]
-_GaussPulseTime: TypeAlias = _ArrayLikeFloat | Literal["cutoff"]
 
 # Type vars to annotate `chirp`
 _NBT1 = TypeVar("_NBT1", bound=npt.NBitBase)
@@ -30,81 +29,6 @@ _ChirpMethod: TypeAlias = Literal["linear", "quadratic", "logarithmic", "hyperbo
 
 def sawtooth(t: _ArrayLikeFloat, width: _ArrayLikeFloat = 1) -> _Array_f8: ...
 def square(t: _ArrayLikeFloat, duty: _ArrayLikeFloat = 0.5) -> _Array_f8: ...
-
-#
-@overload  # retquad: False = ..., retenv: False = ...
-def gausspulse(
-    t: _GaussPulseTime,
-    fc: onp.ToFloat = 1000,
-    bw: onp.ToFloat = 0.5,
-    bwr: onp.ToFloat = -6,
-    tpr: onp.ToFloat = -60,
-    retquad: _Falsy = False,
-    retenv: _Falsy = False,
-) -> _Array_f8: ...
-@overload  # retquad: False = ..., retenv: True (keyword)
-def gausspulse(
-    t: _GaussPulseTime,
-    fc: onp.ToFloat = 1000,
-    bw: onp.ToFloat = 0.5,
-    bwr: onp.ToFloat = -6,
-    tpr: onp.ToFloat = -60,
-    retquad: _Falsy = False,
-    *,
-    retenv: _Truthy,
-) -> tuple[_Array_f8, _Array_f8]: ...
-@overload  # retquad: False (positional), retenv: False (positional)
-def gausspulse(
-    t: _GaussPulseTime,
-    fc: onp.ToFloat,
-    bw: onp.ToFloat,
-    bwr: onp.ToFloat,
-    tpr: onp.ToFloat,
-    retquad: _Falsy,
-    retenv: _Truthy,
-) -> tuple[_Array_f8, _Array_f8]: ...
-@overload  # retquad: True (positional), retenv: False = ...
-def gausspulse(
-    t: _GaussPulseTime,
-    fc: onp.ToFloat,
-    bw: onp.ToFloat,
-    bwr: onp.ToFloat,
-    tpr: onp.ToFloat,
-    retquad: _Truthy,
-    retenv: _Falsy = False,
-) -> tuple[_Array_f8, _Array_f8]: ...
-@overload  # retquad: True (keyword), retenv: False = ...
-def gausspulse(
-    t: _GaussPulseTime,
-    fc: onp.ToFloat = 1000,
-    bw: onp.ToFloat = 0.5,
-    bwr: onp.ToFloat = -6,
-    tpr: onp.ToFloat = -60,
-    *,
-    retquad: _Truthy,
-    retenv: _Falsy = False,
-) -> tuple[_Array_f8, _Array_f8]: ...
-@overload  # retquad: True (positional), retenv: True (positional/keyword)
-def gausspulse(
-    t: _GaussPulseTime,
-    fc: onp.ToFloat,
-    bw: onp.ToFloat,
-    bwr: onp.ToFloat,
-    tpr: onp.ToFloat,
-    retquad: _Truthy,
-    retenv: _Truthy,
-) -> tuple[_Array_f8, _Array_f8, _Array_f8]: ...
-@overload  # retquad: True (keyword), retenv: True
-def gausspulse(
-    t: _GaussPulseTime,
-    fc: onp.ToFloat = 1000,
-    bw: onp.ToFloat = 0.5,
-    bwr: onp.ToFloat = -6,
-    tpr: onp.ToFloat = -60,
-    *,
-    retquad: _Truthy,
-    retenv: _Truthy,
-) -> tuple[_Array_f8, _Array_f8, _Array_f8]: ...
 
 #
 @overload  # Static type checking for float values
@@ -148,3 +72,165 @@ def unit_impulse(
     idx: op.CanIndex | Iterable[op.CanIndex] | Literal["mid"] | None,
     dtype: _DTypeLike[_SCT],
 ) -> npt.NDArray[_SCT]: ...
+
+# Overloads for gausspulse when `t` is scalar
+@overload  # retquad: False = ..., retenv: False = ...
+def gausspulse(
+    t: onp.ToFloat,
+    fc: onp.ToFloat = 1000,
+    bw: onp.ToFloat = 0.5,
+    bwr: onp.ToFloat = -6,
+    tpr: onp.ToFloat = -60,
+    retquad: _Falsy = False,
+    retenv: _Falsy = False,
+) -> np.float64: ...
+@overload  # retquad: False = ..., retenv: True (keyword)
+def gausspulse(
+    t: onp.ToFloat,
+    fc: onp.ToFloat = 1000,
+    bw: onp.ToFloat = 0.5,
+    bwr: onp.ToFloat = -6,
+    tpr: onp.ToFloat = -60,
+    retquad: _Falsy = False,
+    *,
+    retenv: _Truthy,
+) -> tuple[np.float64, np.float64]: ...
+@overload  # retquad: False (positional), retenv: False (positional)
+def gausspulse(
+    t: onp.ToFloat,
+    fc: onp.ToFloat,
+    bw: onp.ToFloat,
+    bwr: onp.ToFloat,
+    tpr: onp.ToFloat,
+    retquad: _Falsy,
+    retenv: _Truthy,
+) -> tuple[np.float64, np.float64]: ...
+@overload  # retquad: True (positional), retenv: False = ...
+def gausspulse(
+    t: onp.ToFloat,
+    fc: onp.ToFloat,
+    bw: onp.ToFloat,
+    bwr: onp.ToFloat,
+    tpr: onp.ToFloat,
+    retquad: _Truthy,
+    retenv: _Falsy = False,
+) -> tuple[np.float64, np.float64]: ...
+@overload  # retquad: True (keyword), retenv: False = ...
+def gausspulse(
+    t: onp.ToFloat,
+    fc: onp.ToFloat = 1000,
+    bw: onp.ToFloat = 0.5,
+    bwr: onp.ToFloat = -6,
+    tpr: onp.ToFloat = -60,
+    *,
+    retquad: _Truthy,
+    retenv: _Falsy = False,
+) -> tuple[np.float64, np.float64]: ...
+@overload  # retquad: True (positional), retenv: True (positional/keyword)
+def gausspulse(
+    t: onp.ToFloat,
+    fc: onp.ToFloat,
+    bw: onp.ToFloat,
+    bwr: onp.ToFloat,
+    tpr: onp.ToFloat,
+    retquad: _Truthy,
+    retenv: _Truthy,
+) -> tuple[np.float64, np.float64, np.float64]: ...
+@overload  # retquad: True (keyword), retenv: True
+def gausspulse(
+    t: onp.ToFloat,
+    fc: onp.ToFloat = 1000,
+    bw: onp.ToFloat = 0.5,
+    bwr: onp.ToFloat = -6,
+    tpr: onp.ToFloat = -60,
+    *,
+    retquad: _Truthy,
+    retenv: _Truthy,
+) -> tuple[np.float64, np.float64, np.float64]: ...
+
+# Overloads for `gausspulse` when `t` is a non-scalar array like
+@overload  # retquad: False = ..., retenv: False = ...
+def gausspulse(
+    t: onp.ToFloatND,
+    fc: onp.ToFloat = 1000,
+    bw: onp.ToFloat = 0.5,
+    bwr: onp.ToFloat = -6,
+    tpr: onp.ToFloat = -60,
+    retquad: _Falsy = False,
+    retenv: _Falsy = False,
+) -> _Array_f8: ...
+@overload  # retquad: False = ..., retenv: True (keyword)
+def gausspulse(
+    t: onp.ToFloatND,
+    fc: onp.ToFloat = 1000,
+    bw: onp.ToFloat = 0.5,
+    bwr: onp.ToFloat = -6,
+    tpr: onp.ToFloat = -60,
+    retquad: _Falsy = False,
+    *,
+    retenv: _Truthy,
+) -> tuple[_Array_f8, _Array_f8]: ...
+@overload  # retquad: False (positional), retenv: False (positional)
+def gausspulse(
+    t: onp.ToFloatND,
+    fc: onp.ToFloat,
+    bw: onp.ToFloat,
+    bwr: onp.ToFloat,
+    tpr: onp.ToFloat,
+    retquad: _Falsy,
+    retenv: _Truthy,
+) -> tuple[_Array_f8, _Array_f8]: ...
+@overload  # retquad: True (positional), retenv: False = ...
+def gausspulse(
+    t: onp.ToFloatND,
+    fc: onp.ToFloat,
+    bw: onp.ToFloat,
+    bwr: onp.ToFloat,
+    tpr: onp.ToFloat,
+    retquad: _Truthy,
+    retenv: _Falsy = False,
+) -> tuple[_Array_f8, _Array_f8]: ...
+@overload  # retquad: True (keyword), retenv: False = ...
+def gausspulse(
+    t: onp.ToFloatND,
+    fc: onp.ToFloat = 1000,
+    bw: onp.ToFloat = 0.5,
+    bwr: onp.ToFloat = -6,
+    tpr: onp.ToFloat = -60,
+    *,
+    retquad: _Truthy,
+    retenv: _Falsy = False,
+) -> tuple[_Array_f8, _Array_f8]: ...
+@overload  # retquad: True (positional), retenv: True (positional/keyword)
+def gausspulse(
+    t: onp.ToFloatND,
+    fc: onp.ToFloat,
+    bw: onp.ToFloat,
+    bwr: onp.ToFloat,
+    tpr: onp.ToFloat,
+    retquad: _Truthy,
+    retenv: _Truthy,
+) -> tuple[_Array_f8, _Array_f8, _Array_f8]: ...
+@overload  # retquad: True (keyword), retenv: True
+def gausspulse(
+    t: onp.ToFloatND,
+    fc: onp.ToFloat = 1000,
+    bw: onp.ToFloat = 0.5,
+    bwr: onp.ToFloat = -6,
+    tpr: onp.ToFloat = -60,
+    *,
+    retquad: _Truthy,
+    retenv: _Truthy,
+) -> tuple[_Array_f8, _Array_f8, _Array_f8]: ...
+
+# Overloads for gausspulse when `t` is `"cutoff"`
+@overload  # retquad: False = ..., retenv: False = ...
+def gausspulse(
+    t: Literal["cutoff"],
+    fc: onp.ToFloat = 1000,
+    bw: onp.ToFloat = 0.5,
+    bwr: onp.ToFloat = -6,
+    tpr: onp.ToFloat = -60,
+    retquad: op.CanBool = False,
+    retenv: op.CanBool = False,
+) -> np.float64: ...
--- tests/signal/test_waveforms.pyi
+++ tests/signal/test_waveforms.pyi
@@ -6,35 +6,39 @@ import optype.numpy as onp
 from scipy.signal import gausspulse
 
 _Array_f8: TypeAlias = onp.ArrayND[np.float64]
+_Scalar_f8: TypeAlias = np.float64
 _Falsy: TypeAlias = Literal[0, False]
 _Truthy: TypeAlias = Literal[1, True]
 
-_gauss_pulse_time: onp.ToFloat | onp.ToFloatND | Literal["cutoff"]
+_time_scalar: onp.ToFloat
+_time_array: onp.ToFloatND
+_time_cutoff: Literal["cutoff"]
 _float: onp.ToFloat
 _truthy: _Truthy
 _falsy: _Falsy
 
 # test gausspulse function overloads
-assert_type(gausspulse(_gauss_pulse_time), _Array_f8)
+# `time` as an array
+assert_type(gausspulse(_time_array), _Array_f8)
 # Full positional arguments
-assert_type(gausspulse(_gauss_pulse_time, _float, _float, _float, _float, _falsy, _falsy), _Array_f8)
+assert_type(gausspulse(_time_array, _float, _float, _float, _float, _falsy, _falsy), _Array_f8)
 assert_type(
-    gausspulse(_gauss_pulse_time, _float, _float, _float, _float, _truthy, _falsy),
+    gausspulse(_time_array, _float, _float, _float, _float, _truthy, _falsy),
     tuple[_Array_f8, _Array_f8],
 )
 assert_type(
-    gausspulse(_gauss_pulse_time, _float, _float, _float, _float, _falsy, _truthy),
+    gausspulse(_time_array, _float, _float, _float, _float, _falsy, _truthy),
     tuple[_Array_f8, _Array_f8],
 )
 assert_type(
-    gausspulse(_gauss_pulse_time, _float, _float, _float, _float, _truthy, _truthy),
+    gausspulse(_time_array, _float, _float, _float, _float, _truthy, _truthy),
     tuple[_Array_f8, _Array_f8, _Array_f8],
 )
 # Full keyword arguments
-assert_type(gausspulse(t=_gauss_pulse_time), _Array_f8)
+assert_type(gausspulse(t=_time_array), _Array_f8)
 assert_type(
     gausspulse(
-        t=_gauss_pulse_time,
+        t=_time_array,
         retquad=_falsy,
         retenv=_falsy,
     ),
@@ -42,7 +46,7 @@ assert_type(
 )
 assert_type(
     gausspulse(
-        t=_gauss_pulse_time,
+        t=_time_array,
         retquad=_truthy,
         retenv=_falsy,
     ),
@@ -50,7 +54,7 @@ assert_type(
 )
 assert_type(
     gausspulse(
-        t=_gauss_pulse_time,
+        t=_time_array,
         retquad=_falsy,
         retenv=_truthy,
     ),
@@ -58,7 +62,7 @@ assert_type(
 )
 assert_type(
     gausspulse(
-        t=_gauss_pulse_time,
+        t=_time_array,
         retquad=_truthy,
         retenv=_truthy,
     ),
@@ -67,18 +71,138 @@ assert_type(
 
 # Mixed positional and keyword arguments
 assert_type(
-    gausspulse(_gauss_pulse_time, _float, _float, _float, _float, retquad=_falsy, retenv=_falsy),
+    gausspulse(_time_array, _float, _float, _float, _float, retquad=_falsy, retenv=_falsy),
     _Array_f8,
 )
 assert_type(
-    gausspulse(_gauss_pulse_time, _float, _float, _float, _float, retquad=_truthy, retenv=_falsy),
+    gausspulse(_time_array, _float, _float, _float, _float, retquad=_truthy, retenv=_falsy),
     tuple[_Array_f8, _Array_f8],
 )
 assert_type(
-    gausspulse(_gauss_pulse_time, _float, _float, _float, _float, retquad=_falsy, retenv=_truthy),
+    gausspulse(_time_array, _float, _float, _float, _float, retquad=_falsy, retenv=_truthy),
     tuple[_Array_f8, _Array_f8],
 )
 assert_type(
-    gausspulse(_gauss_pulse_time, _float, _float, _float, _float, retquad=_truthy, retenv=_truthy),
+    gausspulse(_time_array, _float, _float, _float, _float, retquad=_truthy, retenv=_truthy),
     tuple[_Array_f8, _Array_f8, _Array_f8],
 )
+
+# `time` as a scalar
+assert_type(gausspulse(_time_scalar), _Scalar_f8)
+# Full positional arguments
+assert_type(gausspulse(_time_scalar, _float, _float, _float, _float, _falsy, _falsy), _Scalar_f8)
+assert_type(
+    gausspulse(_time_scalar, _float, _float, _float, _float, _truthy, _falsy),
+    tuple[_Scalar_f8, _Scalar_f8],
+)
+assert_type(
+    gausspulse(_time_scalar, _float, _float, _float, _float, _falsy, _truthy),
+    tuple[_Scalar_f8, _Scalar_f8],
+)
+assert_type(
+    gausspulse(_time_scalar, _float, _float, _float, _float, _truthy, _truthy),
+    tuple[_Scalar_f8, _Scalar_f8, _Scalar_f8],
+)
+# Full keyword arguments
+assert_type(gausspulse(t=_time_scalar), _Scalar_f8)
+assert_type(
+    gausspulse(
+        t=_time_scalar,
+        retquad=_falsy,
+        retenv=_falsy,
+    ),
+    _Scalar_f8,
+)
+assert_type(
+    gausspulse(
+        t=_time_scalar,
+        retquad=_truthy,
+        retenv=_falsy,
+    ),
+    tuple[_Scalar_f8, _Scalar_f8],
+)
+assert_type(
+    gausspulse(
+        t=_time_scalar,
+        retquad=_falsy,
+        retenv=_truthy,
+    ),
+    tuple[_Scalar_f8, _Scalar_f8],
+)
+assert_type(
+    gausspulse(
+        t=_time_scalar,
+        retquad=_truthy,
+        retenv=_truthy,
+    ),
+    tuple[_Scalar_f8, _Scalar_f8, _Scalar_f8],
+)
+
+# Mixed positional and keyword arguments
+assert_type(
+    gausspulse(_time_scalar, _float, _float, _float, _float, retquad=_falsy, retenv=_falsy),
+    _Scalar_f8,
+)
+assert_type(
+    gausspulse(_time_scalar, _float, _float, _float, _float, retquad=_truthy, retenv=_falsy),
+    tuple[_Scalar_f8, _Scalar_f8],
+)
+assert_type(
+    gausspulse(_time_scalar, _float, _float, _float, _float, retquad=_falsy, retenv=_truthy),
+    tuple[_Scalar_f8, _Scalar_f8],
+)
+assert_type(
+    gausspulse(_time_scalar, _float, _float, _float, _float, retquad=_truthy, retenv=_truthy),
+    tuple[_Scalar_f8, _Scalar_f8, _Scalar_f8],
+)
+
+# `time` as the literal `"cutoff"`
+assert_type(gausspulse(_time_cutoff), _Scalar_f8)
+# Full positional arguments
+assert_type(gausspulse(_time_cutoff, _float, _float, _float, _float, _falsy, _falsy), _Scalar_f8)
+assert_type(gausspulse(_time_cutoff, _float, _float, _float, _float, _truthy, _falsy), _Scalar_f8)
+assert_type(gausspulse(_time_cutoff, _float, _float, _float, _float, _falsy, _truthy), _Scalar_f8)
+assert_type(gausspulse(_time_cutoff, _float, _float, _float, _float, _truthy, _truthy), _Scalar_f8)
+# Full keyword arguments
+assert_type(gausspulse(t=_time_cutoff), _Scalar_f8)
+assert_type(
+    gausspulse(
+        t=_time_cutoff,
+        retquad=_falsy,
+        retenv=_falsy,
+    ),
+    _Scalar_f8,
+)
+assert_type(
+    gausspulse(
+        t=_time_cutoff,
+        retquad=_truthy,
+        retenv=_falsy,
+    ),
+    _Scalar_f8,
+)
+assert_type(
+    gausspulse(
+        t=_time_cutoff,
+        retquad=_falsy,
+        retenv=_truthy,
+    ),
+    _Scalar_f8,
+)
+assert_type(
+    gausspulse(
+        t=_time_cutoff,
+        retquad=_truthy,
+        retenv=_truthy,
+    ),
+    _Scalar_f8,
+)
+
+# Mixed positional and keyword arguments
+assert_type(
+    gausspulse(_time_cutoff, _float, _float, _float, _float, retquad=_falsy, retenv=_falsy),
+    _Scalar_f8,
+)
+assert_type(gausspulse(_time_cutoff, _float, _float, _float, _float, retquad=_truthy, retenv=_falsy), _Scalar_f8)
+assert_type(gausspulse(_time_cutoff, _float, _float, _float, _float, retquad=_falsy, retenv=_truthy), _Scalar_f8)
+assert_type(gausspulse(_time_cutoff, _float, _float, _float, _float, retquad=_truthy, retenv=_truthy), _Scalar_f8)
