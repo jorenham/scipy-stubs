76785f557713244fee78e1630e461c5c857cc895
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-11-26 02:14:37 +1100

    `signal`: Simplify overloads for `chirp` in `_waveforms.pyi`

--- scipy-stubs/signal/_waveforms.pyi
+++ scipy-stubs/signal/_waveforms.pyi
@@ -1,11 +1,11 @@
 from collections.abc import Iterable
-from typing import Literal, TypeAlias, TypeVar, overload
+from typing import Any, Literal, TypeAlias, TypeVar, overload
 
 import numpy as np
 import numpy.typing as npt
 import optype as op
 import optype.numpy as onp
-from numpy._typing import _ArrayLike, _DTypeLike
+from numpy._typing import _DTypeLike
 from scipy._typing import AnyShape
 
 __all__ = ["chirp", "gausspulse", "sawtooth", "square", "sweep_poly", "unit_impulse"]
@@ -15,41 +15,35 @@ _SCT = TypeVar("_SCT", bound=np.generic)
 _Truthy: TypeAlias = Literal[1, True]
 _Falsy: TypeAlias = Literal[0, False]
 _ArrayLikeFloat: TypeAlias = onp.ToFloat | onp.ToFloatND
+_Array_f: TypeAlias = onp.ArrayND[np.floating[Any]]
 _Array_f8: TypeAlias = onp.ArrayND[np.float64]
 
-# Type vars to annotate `chirp`
-_NBT1 = TypeVar("_NBT1", bound=npt.NBitBase)
-_NBT2 = TypeVar("_NBT2", bound=npt.NBitBase)
-_NBT3 = TypeVar("_NBT3", bound=npt.NBitBase)
-_NBT4 = TypeVar("_NBT4", bound=npt.NBitBase)
-_NBT5 = TypeVar("_NBT5", bound=npt.NBitBase)
-_ChirpScalar: TypeAlias = float | np.floating[_NBT1] | np.integer[_NBT1]
 _ChirpMethod: TypeAlias = Literal["linear", "quadratic", "logarithmic", "hyperbolic"]
 
 def sawtooth(t: _ArrayLikeFloat, width: _ArrayLikeFloat = 1) -> _Array_f8: ...
 def square(t: _ArrayLikeFloat, duty: _ArrayLikeFloat = 0.5) -> _Array_f8: ...
 
 #
-@overload  # Other dtypes default to np.float64
+@overload  # Arrays
 def chirp(
-    t: onp.SequenceND[float],
+    t: onp.ToFloatND,
     f0: onp.ToFloat,
     t1: onp.ToFloat,
     f1: onp.ToFloat,
     method: _ChirpMethod = "linear",
     phi: onp.ToFloat = 0,
     vertex_zero: op.CanBool = True,
-) -> _Array_f8: ...
-@overload  # Static type checking for float values
+) -> _Array_f: ...
+@overload  # Scalars
 def chirp(
-    t: _ArrayLike[np.floating[_NBT1] | np.integer[_NBT1]],
-    f0: _ChirpScalar[_NBT2],
-    t1: _ChirpScalar[_NBT3],
-    f1: _ChirpScalar[_NBT4],
+    t: onp.ToFloat,
+    f0: onp.ToFloat,
+    t1: onp.ToFloat,
+    f1: onp.ToFloat,
     method: _ChirpMethod = "linear",
-    phi: _ChirpScalar[_NBT5] = 0,
+    phi: onp.ToFloat = 0,
     vertex_zero: op.CanBool = True,
-) -> onp.ArrayND[np.floating[_NBT1 | _NBT2 | _NBT3 | _NBT4 | _NBT5]]: ...
+) -> np.floating[Any]: ...
 
 #
 def sweep_poly(
