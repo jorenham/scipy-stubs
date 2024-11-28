2691cc8d3fadc61da5c186f1d2698a3213da36aa
jorenham <jhammudoglu@gmail.com>
2024-11-28 23:08:26 +0100

    ✨ complete `scipy.optimize.dual_annealing`

--- scipy-stubs/optimize/_dual_annealing.pyi
+++ scipy-stubs/optimize/_dual_annealing.pyi
@@ -1,35 +1,56 @@
-from typing import type_check_only
+from collections.abc import Callable
+from typing import Concatenate, Literal, TypeAlias, TypedDict, type_check_only
 
 import numpy as np
 import optype.numpy as onp
-from scipy._typing import Untyped
+from scipy._typing import Seed
 from scipy.optimize import OptimizeResult as _OptimizeResult
+from ._constraints import Bounds
+from ._hessian_update_strategy import HessianUpdateStrategy
+from ._minimize import _MinimizeOptions
+from ._typing import Constraints, MethodMimimize
 
 __all__ = ["dual_annealing"]
 
+_Float1D: TypeAlias = onp.Array1D[np.float64]
+_FDMethod: TypeAlias = Literal["2-point", "3-point", "cs"]
+
 @type_check_only
+class _MinimizeKwargs(TypedDict, total=False):
+    method: MethodMimimize
+    jac: Callable[Concatenate[_Float1D, ...], onp.ToFloat1D] | _FDMethod | bool
+    hess: Callable[Concatenate[_Float1D, ...], onp.ToFloat2D] | _FDMethod | HessianUpdateStrategy
+    hessp: Callable[Concatenate[_Float1D, _Float1D, ...], onp.ToFloat1D]
+    constraints: Constraints
+    tol: onp.ToFloat
+    options: _MinimizeOptions
+
+###
+
 class OptimizeResult(_OptimizeResult):
-    x: onp.Array1D[np.float64]
-    fun: float
-    status: int
     success: bool
-    message: str
+    status: int
+    x: _Float1D
+    fun: float
     nit: int
     nfev: int
+    njev: int
+    nhev: int
+    message: str
 
 def dual_annealing(
-    func: Untyped,
-    bounds: Untyped,
-    args: Untyped = (),
-    maxiter: int = 1000,
-    minimizer_kwargs: Untyped | None = None,
-    initial_temp: float = 5230.0,
-    restart_temp_ratio: float = 2e-05,
-    visit: float = 2.62,
-    accept: float = -5.0,
-    maxfun: float = 10000000.0,
-    seed: Untyped | None = None,
-    no_local_search: bool = False,
-    callback: Untyped | None = None,
-    x0: Untyped | None = None,
+    func: Callable[Concatenate[_Float1D, ...], onp.ToFloat],
+    bounds: Bounds | tuple[onp.ToFloat1D, onp.ToFloat1D] | onp.ToFloat2D,
+    args: tuple[object, ...] = (),
+    maxiter: onp.ToJustInt = 1_000,
+    minimizer_kwargs: _MinimizeKwargs | None = None,
+    initial_temp: onp.ToFloat = 5_230.0,
+    restart_temp_ratio: onp.ToFloat = 2e-05,
+    visit: onp.ToFloat = 2.62,
+    accept: onp.ToFloat = -5.0,
+    maxfun: onp.ToFloat = 10_000_000.0,
+    seed: Seed | None = None,
+    no_local_search: onp.ToBool = False,
+    callback: Callable[[_Float1D, float, Literal[0, 1, 2]], bool | None] | None = None,
+    x0: onp.ToFloat1D | None = None,
 ) -> _OptimizeResult: ...
