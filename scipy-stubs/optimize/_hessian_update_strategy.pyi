2b451b163e97be5b0073c5929f57e003a09a011f
jorenham <jhammudoglu@gmail.com>
2024-11-28 04:00:44 +0100

    ✨ complete `scipy.optimize._hessian_update_strategy`

--- scipy-stubs/optimize/_hessian_update_strategy.pyi
+++ scipy-stubs/optimize/_hessian_update_strategy.pyi
@@ -1,40 +1,50 @@
-from typing import Final, Literal
+from typing import Final, Literal, Protocol, TypeAlias, overload
 
-from scipy._typing import Untyped, UntypedArray
+import numpy as np
+import optype.numpy as onp
 
 __all__ = ["BFGS", "SR1", "HessianUpdateStrategy"]
 
-class HessianUpdateStrategy:
-    def initialize(self, /, n: int, approx_type: Literal["hess", "inv_hess"]) -> None: ...
-    def update(self, /, delta_x: UntypedArray, delta_grad: UntypedArray) -> None: ...
-    def dot(self, /, p: Untyped) -> UntypedArray: ...
-    def get_matrix(self, /) -> UntypedArray: ...
+_HessApprox: TypeAlias = Literal["hess", "inv_hess"]
+_ExceptionStrategy: TypeAlias = Literal["skip_update", "damp_update"]
+_ToInitScale: TypeAlias = onp.ToFloat | onp.ToFloat2D | Literal["auto"]
+
+###
+
+# NOTE: this isn't a Protocol in practice, but it avoids having to deal with`abc`
+class HessianUpdateStrategy(Protocol):
+    def get_matrix(self, /) -> onp.Array2D[np.float64]: ...
+    def initialize(self, /, n: int, approx_type: _HessApprox) -> None: ...
+    def update(self, /, delta_x: onp.Array1D[np.float64], delta_grad: onp.Array1D[np.float64]) -> None: ...
+    @overload
+    def dot(self, /, p: onp.ToFloat1D) -> onp.Array1D[np.float64]: ...
+    @overload
+    def dot(self, /, p: onp.ToComplex1D) -> onp.Array1D[np.float64 | np.complex128]: ...
 
 class FullHessianUpdateStrategy(HessianUpdateStrategy):
-    init_scale: Untyped
-    first_iteration: Untyped
-    approx_type: Untyped
-    B: Untyped
-    H: Untyped
-    n: Untyped
-    def __init__(self, /, init_scale: Literal["auto"] | float | UntypedArray = "auto") -> None: ...
+    init_scale: Final[_ToInitScale]
+
+    B: onp.Array2D[np.float64] | None
+    H: onp.Array2D[np.float64] | None
+    first_iteration: bool | None
+    approx_type: _HessApprox | None
+    n: int  # set in `initialize`
+
+    def __init__(self, /, init_scale: _ToInitScale = "auto") -> None: ...
 
 class BFGS(FullHessianUpdateStrategy):
+    exception_strategy: Final[_ExceptionStrategy]
     min_curvature: Final[float]
-    exception_strategy: Final[Literal["skip_update", "damp_update"]]
+
     def __init__(
         self,
         /,
-        exception_strategy: Literal["skip_update", "damp_update"] = "skip_update",
+        exception_strategy: _ExceptionStrategy = "skip_update",
         min_curvature: float | None = None,
-        init_scale: Literal["auto"] | float | UntypedArray = "auto",
+        init_scale: _ToInitScale = "auto",
     ) -> None: ...
 
 class SR1(FullHessianUpdateStrategy):
     min_denominator: Final[float]
-    def __init__(
-        self,
-        /,
-        min_denominator: float = 1e-08,
-        init_scale: Literal["auto"] | float | UntypedArray = "auto",
-    ) -> None: ...
+
+    def __init__(self, /, min_denominator: float = 1e-08, init_scale: _ToInitScale = "auto") -> None: ...
