aa3372138cb381af401fc267bc7c55a301b594a0
jorenham <jhammudoglu@gmail.com>
2024-11-29 03:57:37 +0100

    ✨ complete `optimize._shgo_lib._vertex`

--- scipy-stubs/optimize/_shgo_lib/_vertex.pyi
+++ scipy-stubs/optimize/_shgo_lib/_vertex.pyi
@@ -1,135 +1,126 @@
 import abc
 from collections import OrderedDict
 from collections.abc import Callable, Iterable, Iterator, Sequence
-from typing import Any, Final, Generic, TypeAlias
-from typing_extensions import Self, TypeVar, override
+from typing import Concatenate, Final, Generic, TypeAlias, type_check_only
+from typing_extensions import TypeVar
 
 import numpy as np
 import optype.numpy as onp
-from scipy._lib._util import MapWrapper as MapWrapper
-from scipy._typing import Untyped, UntypedTuple
 
-_SCT = TypeVar("_SCT", bound=np.number[Any], default=np.floating[Any])
-_SCT_co = TypeVar("_SCT_co", bound=np.number[Any], covariant=True, default=np.floating[Any])
+_VT_co = TypeVar("_VT_co", bound=VertexBase, default=VertexBase, covariant=True)
 
-_Vector: TypeAlias = onp.Array1D[_SCT]
-_VectorLike: TypeAlias = tuple[float | _SCT, ...] | _Vector[_SCT]
+_Float1D: TypeAlias = onp.Array1D[np.float64]
+_Fun0D: TypeAlias = Callable[Concatenate[_Float1D, ...], onp.ToFloat]
+_Fun1D: TypeAlias = Callable[Concatenate[_Float1D, ...], onp.ToFloat1D]
 
-_ScalarField: TypeAlias = Callable[[_Vector[_SCT]], float | _SCT]
-_VectorField: TypeAlias = Callable[[_Vector[_SCT]], _Vector[_SCT]]
-_ScalarFieldCons: TypeAlias = Callable[[_Vector[_SCT]], Untyped]  # TODO
+@type_check_only
+class _VertexMixin:
+    def connect(self, /, v: VertexBase) -> None: ...
+    def disconnect(self, /, v: VertexBase) -> None: ...
 
-class VertexBase(Generic[_SCT_co], metaclass=abc.ABCMeta):
-    x: _VectorLike[_SCT_co]
-    x_a: _Vector[_SCT_co]  # lazy
+###
+
+class VertexBase(abc.ABC):
+    x: onp.ToFloat1D
+    x_a: _Float1D  # lazy
     hash: Final[int]
     index: Final[int | None]
-    nn: set[Self]
-
-    st: set[Self]  # might not be set
+    nn: set[VertexBase]
+    st: set[VertexBase]  # might not be set
     feasible: bool  # might not be set
 
-    def __init__(self, /, x: _VectorLike[_SCT_co], nn: Iterable[Self] | None = None, index: int | None = None) -> None: ...
+    def __init__(self, /, x: onp.ToFloat1D, nn: Iterable[VertexBase] | None = None, index: int | None = None) -> None: ...
     @abc.abstractmethod
-    def connect(self, /, v: Self) -> None: ...
+    def connect(self, /, v: VertexBase) -> None: ...
     @abc.abstractmethod
-    def disconnect(self, /, v: Self) -> None: ...
-    def star(self, /) -> set[Self]: ...
+    def disconnect(self, /, v: VertexBase) -> None: ...
+    def star(self, /) -> set[VertexBase]: ...
 
-class VertexScalarField(VertexBase[_SCT_co], Generic[_SCT_co]):
+class VertexScalarField(_VertexMixin, VertexBase):
     check_min: bool
     check_max: bool
-    # TODO: Support non-empty `field_args` and `g_cons_args`
+
     def __init__(
         self,
         /,
-        x: _VectorLike[_SCT_co],
-        field: _ScalarField[_SCT_co] | None = None,
-        nn: Iterable[Self] | None = None,
+        x: onp.ToFloat1D,
+        field: _Fun0D | None = None,
+        nn: Iterable[VertexBase] | None = None,
         index: int | None = None,
-        field_args: tuple[()] = (),
-        g_cons: _ScalarFieldCons[_SCT_co] | None = None,
-        g_cons_args: tuple[()] = (),
+        field_args: tuple[object, ...] = (),
+        g_cons: _Fun1D | None = None,
+        g_cons_args: tuple[object, ...] = (),
     ) -> None: ...
-    @override
-    def connect(self, /, v: Self) -> None: ...
-    @override
-    def disconnect(self, /, v: Self) -> None: ...
+    #
     def minimiser(self, /) -> bool: ...
     def maximiser(self, /) -> bool: ...
 
-class VertexVectorField(VertexBase[_SCT_co], Generic[_SCT_co], metaclass=abc.ABCMeta):
+class VertexVectorField(_VertexMixin, VertexBase):
     # NOTE: The implementaiton is a WIP
-    # TODO: Support non-empty `field_args`, `vfield_args`, and `g_cons_args`
     def __init__(
         self,
         /,
-        x: _VectorLike[_SCT_co],
-        sfield: _ScalarField[_SCT_co] | None = None,
-        vfield: _VectorField[_SCT_co] | None = None,
-        field_args: tuple[()] = (),
-        vfield_args: tuple[()] = (),
-        g_cons: _ScalarFieldCons[_SCT_co] | None = None,
-        g_cons_args: tuple[()] = (),
-        nn: Iterable[Self] | None = None,
+        x: onp.ToFloat1D,
+        sfield: _Fun0D | None = None,
+        vfield: _Fun1D | None = None,
+        field_args: tuple[object, ...] = (),
+        vfield_args: tuple[object, ...] = (),
+        g_cons: _Fun1D | None = None,
+        g_cons_args: tuple[object, ...] = (),
+        nn: Iterable[VertexBase] | None = None,
         index: int | None = None,
     ) -> None: ...
-    @override
-    def connect(self, /, v: Self) -> None: ...
-    @override
-    def disconnect(self, /, v: Self) -> None: ...
 
-class VertexCube(VertexBase[_SCT_co], Generic[_SCT_co]):
-    def __init__(self, /, x: _VectorLike[_SCT_co], nn: Iterable[Self] | None = None, index: int | None = None) -> None: ...
-    @override
-    def connect(self, /, v: Self) -> None: ...
-    @override
-    def disconnect(self, /, v: Self) -> None: ...
+class VertexCube(_VertexMixin, VertexBase):
+    def __init__(self, /, x: onp.ToFloat1D, nn: Iterable[VertexBase] | None = None, index: int | None = None) -> None: ...
 
-_KT = TypeVar("_KT", default=Untyped)  # TODO: Select a decent default
-_VT = TypeVar("_VT", bound=VertexBase, default=VertexBase)
-
-class VertexCacheBase(Generic[_KT, _VT]):
-    cache: OrderedDict[_KT, _VT]
+class VertexCacheBase(Generic[_VT_co]):
+    cache: OrderedDict[onp.ToFloat1D, _VT_co]
     nfev: int
     index: int
+
     def __init__(self, /) -> None: ...
-    def __iter__(self, /) -> Iterator[_VT]: ...
+    def __iter__(self, /) -> Iterator[_VT_co]: ...
     def size(self, /) -> int: ...
     def print_out(self, /) -> None: ...
 
-class VertexCacheIndex(VertexCacheBase[_KT, _VT], Generic[_KT, _VT]):
-    Vertex: type[_VT]
-    def __getitem__(self, x: _KT, /, nn: None = None) -> _VT: ...
+class VertexCacheIndex(VertexCacheBase[VertexCube]):
+    Vertex: Final[type[VertexCube]]
 
-class VertexCacheField(VertexCacheBase[_KT, _VT], Generic[_KT, _VT, _SCT_co]):
+    def __getitem__(self, x: onp.ToFloat1D, /, nn: None = None) -> VertexCube: ...
+
+class VertexCacheField(VertexCacheBase[VertexScalarField]):
+    Vertex: Final[type[VertexScalarField]]
+
+    field: Final[_Fun0D]
+    field_args: Final[tuple[object, ...]]
+    wfield: Final[FieldWrapper]
+    fpool: Final[set[VertexScalarField]]
+    process_fpool: Final[Callable[[], None]]
+
+    g_cons: Final[Sequence[_Fun1D]]
+    g_cons_args: Final[tuple[object, ...]]
+    wgcons: Final[ConstraintWrapper]
+    gpool: Final[set[VertexScalarField]]
+    process_gpool: Final[Callable[[], None]]
+
+    workers: Final[int]
     index: int
-    Vertex: type[_VT]
-    field: _ScalarField[_SCT_co]
-    field_args: tuple[()]
-    wfield: FieldWrapper[_SCT_co]
-    g_cons: Sequence[_ScalarFieldCons[_SCT_co]]
-    g_cons_args: tuple[()]
-    wgcons: ConstraintWrapper[_SCT_co]
-    gpool: set[UntypedTuple]
-    fpool: set[UntypedTuple]
     sfc_lock: bool
-    workers: int
-    process_gpool: Callable[[], None]
-    process_fpool: Callable[[], None]
+
     def __init__(
         self,
         /,
-        field: Untyped | None = None,
-        field_args: tuple[()] = (),
-        g_cons: Sequence[_ScalarFieldCons[_SCT_co]] | None = None,
-        g_cons_args: tuple[()] = (),
+        field: _Fun0D | _Fun1D | None = None,
+        field_args: tuple[object, ...] = (),
+        g_cons: Sequence[_Fun1D] | None = None,
+        g_cons_args: tuple[object, ...] = (),
         workers: int = 1,
     ) -> None: ...
-    def __getitem__(self, x: _KT, /, nn: Iterable[_VT] | None = None) -> _VT: ...
+    def __getitem__(self, x: onp.ToFloat1D, /, nn: Iterable[VertexBase] | None = None) -> VertexScalarField: ...
     def process_pools(self, /) -> None: ...
-    def feasibility_check(self, /, v: _VT) -> bool: ...
-    def compute_sfield(self, /, v: _VT) -> None: ...
+    def feasibility_check(self, /, v: VertexBase) -> bool: ...
+    def compute_sfield(self, /, v: VertexBase) -> None: ...
     def proc_gpool(self, /) -> None: ...
     def pproc_gpool(self, /) -> None: ...
     def proc_fpool_g(self, /) -> None: ...
@@ -138,15 +129,16 @@ class VertexCacheField(VertexCacheBase[_KT, _VT], Generic[_KT, _VT, _SCT_co]):
     def pproc_fpool_nog(self, /) -> None: ...
     def proc_minimisers(self, /) -> None: ...
 
-class ConstraintWrapper(Generic[_SCT]):
-    g_cons: Sequence[_ScalarFieldCons[_SCT]]
-    g_cons_args: Sequence[UntypedTuple]
-    def __init__(self, /, g_cons: Sequence[_ScalarFieldCons[_SCT]], g_cons_args: Sequence[UntypedTuple]) -> None: ...
-    def gcons(self, /, v_x_a: _Vector[_SCT]) -> bool: ...
+class ConstraintWrapper:
+    g_cons: Sequence[_Fun1D]
+    g_cons_args: Sequence[tuple[object, ...]]
 
-class FieldWrapper(Generic[_SCT]):
-    # TODO: Support non-empty `g_cons_args`
-    field: _ScalarField[_SCT] | _VectorField[_SCT]
-    field_args: tuple[()]
-    def __init__(self, /, field: _ScalarField[_SCT] | _VectorField[_SCT], field_args: tuple[()]) -> None: ...
-    def func(self, /, v_x_a: _Vector[_SCT]) -> _SCT: ...
+    def __init__(self, /, g_cons: Sequence[_Fun1D], g_cons_args: Sequence[tuple[object, ...]]) -> None: ...
+    def gcons(self, /, v_x_a: _Float1D) -> bool: ...
+
+class FieldWrapper:
+    field: _Fun0D | _Fun1D
+    field_args: tuple[object, ...]
+
+    def __init__(self, /, field: _Fun0D | _Fun1D, field_args: tuple[object, ...]) -> None: ...
+    def func(self, /, v_x_a: _Float1D) -> onp.ToFloat | onp.ToFloat1D: ...
