e236561b69b73279835b06a3d352121848924931
jorenham <jhammudoglu@gmail.com>
2024-11-29 02:42:36 +0100

    ✨ complete `optimize._dcsrch` (private)

--- scipy-stubs/optimize/_dcsrch.pyi
+++ scipy-stubs/optimize/_dcsrch.pyi
@@ -1,58 +1,67 @@
-from scipy._typing import Untyped
+from collections.abc import Callable
+from typing import Final, TypeAlias
+
+import optype.numpy as onp
+
+_Fun: TypeAlias = Callable[[float], onp.ToFloat]
+
+###
 
 class DCSRCH:
-    stage: Untyped
-    ginit: Untyped
-    gtest: Untyped
-    gx: Untyped
-    gy: Untyped
-    finit: Untyped
-    fx: Untyped
-    fy: Untyped
-    stx: Untyped
-    sty: Untyped
-    stmin: Untyped
-    stmax: Untyped
-    width: Untyped
-    width1: Untyped
-    ftol: Untyped
-    gtol: Untyped
-    xtol: Untyped
-    stpmin: Untyped
-    stpmax: Untyped
-    phi: Untyped
-    derphi: Untyped
+    phi: Final[_Fun]
+    derphi: Final[_Fun]
+    ftol: Final[float]
+    gtol: Final[float]
+    xtol: Final[float]
+    stpmin: Final[float]
+    stpmax: Final[float]
+
+    stage: int | None
+    ginit: float | None
+    gtest: float | None
+    gx: float | None
+    gy: float | None
+    finit: float | None
+    fx: float | None
+    fy: float | None
+    stx: float | None
+    sty: float | None
+    stmin: float | None
+    stmax: float | None
+    width: float | None
+    width1: float | None
+
     def __init__(
         self,
         /,
-        phi: Untyped,
-        derphi: Untyped,
-        ftol: Untyped,
-        gtol: Untyped,
-        xtol: Untyped,
-        stpmin: Untyped,
-        stpmax: Untyped,
+        phi: _Fun,
+        derphi: _Fun,
+        ftol: float,
+        gtol: float,
+        xtol: float,
+        stpmin: float,
+        stpmax: float,
     ) -> None: ...
     def __call__(
         self,
         /,
-        alpha1: Untyped,
-        phi0: Untyped | None = None,
-        derphi0: Untyped | None = None,
+        alpha1: float,
+        phi0: float | None = None,
+        derphi0: float | None = None,
         maxiter: int = 100,
-    ) -> Untyped: ...
+    ) -> tuple[float | None, float, float, bytes]: ...  # alpha, phi(alpha), phi(0), task
 
 def dcstep(
-    stx: Untyped,
-    fx: Untyped,
-    dx: Untyped,
-    sty: Untyped,
-    fy: Untyped,
-    dy: Untyped,
-    stp: Untyped,
-    fp: Untyped,
-    dp: Untyped,
-    brackt: Untyped,
-    stpmin: Untyped,
-    stpmax: Untyped,
-) -> Untyped: ...
+    stx: float,
+    fx: float,
+    dx: float,
+    sty: float,
+    fy: float,
+    dy: float,
+    stp: float,
+    fp: float,
+    dp: float,
+    brackt: bool,
+    stpmin: float,
+    stpmax: float,
+) -> tuple[float, float, float, float, float, float, float, bool]: ...  # stx, fx, dx, sty, fy, dy, stp, brackt
