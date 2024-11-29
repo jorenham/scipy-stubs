0cd6efbba2c030598ec4de972a9d85a924ad372f
jorenham <jhammudoglu@gmail.com>
2024-11-29 02:51:01 +0100

    ✨ complete `optimize.linear_sum_assignment`

--- scipy-stubs/optimize/_lsap.pyi
+++ scipy-stubs/optimize/_lsap.pyi
@@ -1,3 +1,7 @@
-from scipy._typing import Untyped
+import numpy as np
+import optype.numpy as onp
 
-def linear_sum_assignment(*args: Untyped, **kwargs: Untyped) -> Untyped: ...
+def linear_sum_assignment(
+    cost_matrix: onp.ToFloat2D,
+    maximize: onp.ToBool = False,
+) -> tuple[onp.Array1D[np.intp], onp.Array1D[np.intp]]: ...
