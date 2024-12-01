6a3394b0f33eae50795924eaf30ac365c80b7102
jorenham <jhammudoglu@gmail.com>
2024-12-01 17:04:51 +0100

    ✨ complete `optimize._trustregion`

--- scipy-stubs/optimize/_trustregion.pyi
+++ scipy-stubs/optimize/_trustregion.pyi
@@ -1,7 +1,8 @@
 import abc
+from collections.abc import Callable
 
 import numpy as np
-from scipy._typing import Untyped
+import optype.numpy as onp
 
 __all__: list[str] = []
 
@@ -9,22 +10,34 @@ class BaseQuadraticSubproblem:
     def __init__(
         self,
         /,
-        x: Untyped,
-        fun: Untyped,
-        jac: Untyped,
-        hess: Untyped | None = None,
-        hessp: Untyped | None = None,
+        x: onp.ToFloat1D,
+        fun: Callable[[onp.Array1D[np.float64]], onp.ToFloat],
+        jac: Callable[[onp.Array1D[np.float64]], onp.ToFloat1D],
+        hess: Callable[[onp.Array1D[np.float64]], onp.ToFloat2D] | None = None,
+        hessp: Callable[[onp.Array1D[np.float64], onp.Array1D[np.float64]], onp.ToFloat1D] | None = None,
     ) -> None: ...
-    def __call__(self, /, p: Untyped) -> Untyped: ...
+    def __call__(self, /, p: onp.ToFloat1D) -> float | np.float64: ...
+
+    #
     @property
-    def fun(self, /) -> Untyped: ...
+    def fun(self, /) -> float | np.float64: ...
     @property
-    def jac(self, /) -> Untyped: ...
+    def jac_mag(self, /) -> float | np.float64: ...
     @property
-    def hess(self, /) -> Untyped: ...
-    def hessp(self, /, p: Untyped) -> Untyped: ...
+    def jac(self, /) -> onp.Array1D[np.float64]: ...
     @property
-    def jac_mag(self, /) -> Untyped: ...
-    def get_boundaries_intersections(self, /, z: Untyped, d: Untyped, trust_radius: Untyped) -> Untyped: ...
+    def hess(self, /) -> onp.Array2D[np.float64]: ...
+
+    #
+    def hessp(self, /, p: onp.ToFloat1D) -> onp.Array1D[np.float64]: ...
+    def get_boundaries_intersections(
+        self,
+        /,
+        z: onp.ToArray1D,
+        d: onp.ToArray1D,
+        trust_radius: onp.ToFloat,
+    ) -> list[float | np.float64]: ...  # list of size 2
+
+    #
     @abc.abstractmethod
-    def solve(self, /, trust_radius: float | np.float64) -> Untyped: ...
+    def solve(self, /, trust_radius: onp.ToFloat) -> tuple[onp.Array1D[np.float64], bool]: ...
