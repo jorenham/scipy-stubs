bdc70e6c6871d7961b3065f919231f4f90aec500
jorenham <jhammudoglu@gmail.com>
2024-12-01 18:20:14 +0100

    ✨ complete `optimize._group_columns*`

--- scipy-stubs/optimize/_group_columns.pyi
+++ scipy-stubs/optimize/_group_columns.pyi
@@ -1,9 +1,20 @@
+# https://github.com/scipy/scipy/blob/v1.14.1/scipy/optimize/_group_columns.py
+
 from typing import Final
 from typing_extensions import LiteralString
 
-from scipy._typing import Untyped
+import numpy as np
+import optype.numpy as onp
 
 __pythran__: Final[tuple[LiteralString, LiteralString]]
 
-def group_dense(m: Untyped, n: Untyped, A: Untyped) -> Untyped: ...
-def group_sparse(m: Untyped, n: Untyped, indices: Untyped, indptr: Untyped) -> Untyped: ...
+# (int, int, int[:, :]) -> int[:]
+def group_dense(m: onp.ToJustInt, n: onp.ToJustInt, A: onp.ToJustInt2D) -> onp.Array1D[np.int32]: ...
+
+# (int, int, int[:], int[:]) -> int[:]
+def group_sparse(
+    m: onp.ToJustInt,
+    n: onp.ToJustInt,
+    indices: onp.ToJustInt1D,
+    indptr: onp.ToJustInt1D,
+) -> onp.Array1D[np.int32]: ...
