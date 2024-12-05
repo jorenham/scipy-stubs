69a750c3a958adf4587d98ff8721c3d0982e79a4
miloth <milo.knightword@gmail.com>
2024-12-05 02:23:45 +0000

    :recycle: moved `scripts` to `importlib`

--- scripts/version_bounds.py
+++ scripts/version_bounds.py
@@ -1,46 +1,18 @@
 """Script for the fucntions fetching the minimum versions."""
 
-import re
-import sys
-from pathlib import Path
-from typing import Any
-
-if sys.version_info >= (3, 11):
-    import tomllib
-else:  # TODO: remove this once 3.10 is dropped!
-    import tomli as tomllib
+import importlib.metadata
 
 
-def get_pyproject() -> dict[str, Any]:
-    """Load the pyproject.toml as a dictionary."""
-    with Path("pyproject.toml").open("rb") as f:
-        return tomllib.load(f)
-
-
-def get_minimum_python() -> str:
+def scipy_minimum_python() -> str:
     """Fetch the minimum Python version specified in pyproject.toml."""
-    raw_version = get_pyproject()["project"]["requires-python"]
+    raw_version = importlib.metadata.metadata("scipy")["Requires-Python"]
     if "<" in raw_version:
         raise NotImplementedError("Version specifier with upper bound not yet supported!")
-    return re.sub(r"[>=~]", "", raw_version)
+    return raw_version.replace(">=", "").replace("~=", "")
 
 
-def get_minimum_numpy() -> str:
+def scipy_minimum_numpy() -> str:
     """Fetch the minimum numpy version required by the current scipy package."""
-    scipy_group = get_pyproject()["dependency-groups"]["scipy"]
-    scipy_version = next(dep for dep in scipy_group if dep.startswith("scipy==")).replace("scipy==", "")
-
-    import requests  # Import only when needed, leaving only standard libs at the top  # noqa: PLC0415
-
-    response = requests.get(
-        f"https://raw.githubusercontent.com/scipy/scipy/refs/tags/v{scipy_version}/pyproject.toml",
-        timeout=10,
-    )
-    scipy_pyproject = tomllib.loads(response.text)
-
-    numpy_minimum_dep = next(dep for dep in scipy_pyproject["project"]["dependencies"] if dep.startswith("numpy>="))
-    numpy_minimum_dep = numpy_minimum_dep.replace("numpy>=", "")
-    if ",<" in numpy_minimum_dep:  # Remove any upper dep if specified
-        numpy_minimum_dep = numpy_minimum_dep.split(",")[0]
-
-    return numpy_minimum_dep
+    np_minimum_dep = next(req for req in importlib.metadata.requires("scipy") if req.startswith("numpy") and " extra " not in req)
+    np_minimum_dep = next(ver for ver in np_minimum_dep.split(",") if ">" in ver)
+    return np_minimum_dep.replace("numpy", "").replace(">=", "").replace(">", "")
