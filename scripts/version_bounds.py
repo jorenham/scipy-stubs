c48ee75e4fcc102b7301b198a80783e0e3b01ce6
Joren Hammudoglu <jhammudoglu@gmail.com>
2024-12-09 21:16:22 +0100

    💚 fix `typetest-oldest-supported-numpy` CI job (#289)
    * 💚 fix `typetest-oldest-supported-numpy` CI job
    
    * 🚨 fix mypy error in `scripts/version_bounds.py`

--- scripts/version_bounds.py
+++ scripts/version_bounds.py
@@ -14,7 +14,8 @@ def scipy_minimum_python() -> str:
 
 def scipy_minimum_numpy() -> str:
     """Fetch the minimum numpy version required by the current scipy package."""
-    np_minimum_dep = next(req for req in importlib.metadata.requires("scipy") if req.startswith("numpy") and " extra " not in req)
+    reqs = importlib.metadata.requires("scipy") or []
+    np_minimum_dep = next(req for req in reqs if req.startswith("numpy") and " extra " not in req)
     np_minimum_dep = next(ver for ver in np_minimum_dep.split(",") if ">" in ver)
     return np_minimum_dep.replace("numpy", "").replace(">=", "").replace(">", "")
 
