d29dcf9b61510f2c4191406689520b16f3bb7532
jorenham <jhammudoglu@gmail.com>
2024-12-15 23:07:24 +0100

    ✨ `sparse`: complete `linalg.minres`

--- scipy-stubs/sparse/linalg/_isolve/minres.pyi
+++ scipy-stubs/sparse/linalg/_isolve/minres.pyi
@@ -1,17 +1,34 @@
-from scipy._typing import Untyped
+from collections.abc import Callable
+from typing import Any, TypeAlias
+from typing_extensions import TypeVar
+
+import numpy as np
+import optype.numpy as onp
+from scipy.sparse._base import _spbase
+from scipy.sparse._typing import Scalar
+from scipy.sparse.linalg import LinearOperator
 
 __all__ = ["minres"]
 
+_FloatT = TypeVar("_FloatT", bound=np.float32 | np.float64, default=np.float64)
+_ScalarT = TypeVar("_ScalarT", bound=Scalar)
+
+_Ignored: TypeAlias = object
+_ToInt: TypeAlias = np.integer[Any] | np.bool_
+_ToLinearOperator: TypeAlias = onp.CanArrayND[_ScalarT] | _spbase[_ScalarT] | LinearOperator[_ScalarT]
+
+###
+
 def minres(
-    A: Untyped,
-    b: Untyped,
-    x0: Untyped | None = None,
+    A: _ToLinearOperator[_FloatT | _ToInt],
+    b: onp.ToFloat1D,
+    x0: onp.ToFloat1D | None = None,
     *,
-    rtol: float = 1e-05,
-    shift: float = 0.0,
-    maxiter: Untyped | None = None,
-    M: Untyped | None = None,
-    callback: Untyped | None = None,
-    show: bool = False,
-    check: bool = False,
-) -> Untyped: ...
+    rtol: onp.ToFloat = 1e-5,
+    atol: onp.ToFloat = 0.0,
+    maxiter: int | None = None,
+    M: _ToLinearOperator[_FloatT | _ToInt] | None = None,
+    callback: Callable[[onp.Array1D[_FloatT]], _Ignored] | None = None,
+    show: onp.ToBool = False,
+    check: onp.ToBool = False,
+) -> tuple[onp.Array1D[_FloatT], int]: ...
