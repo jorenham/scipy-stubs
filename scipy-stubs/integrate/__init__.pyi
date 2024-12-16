97ce947245cc41b8e6743f28b28082baeafaa0e0
jorenham <jhammudoglu@gmail.com>
2024-12-16 20:06:49 +0100

    👽️ `scipy-stubs`: fix new stubtest errors

--- scipy-stubs/integrate/__init__.pyi
+++ scipy-stubs/integrate/__init__.pyi
@@ -1,22 +1,14 @@
 from . import dop, lsoda, odepack, quadpack, vode  # deprecated namespaces
 from ._bvp import solve_bvp
-from ._ivp import (
-    BDF,
-    DOP853,
-    LSODA,
-    RK23,
-    RK45,
-    DenseOutput,
-    OdeSolution,
-    OdeSolver,
-    Radau,
-    solve_ivp,
-)
-from ._ode import *
-from ._odepack_py import *
+from ._cubature import cubature
+from ._ivp import BDF, DOP853, LSODA, RK23, RK45, DenseOutput, OdeSolution, OdeSolver, Radau, solve_ivp
+from ._lebedev import lebedev_rule
+from ._ode import complex_ode, ode
+from ._odepack_py import ODEintWarning, odeint
 from ._quad_vec import quad_vec
-from ._quadpack_py import *
-from ._quadrature import *
+from ._quadpack_py import IntegrationWarning, dblquad, nquad, quad, tplquad
+from ._quadrature import cumulative_simpson, cumulative_trapezoid, fixed_quad, newton_cotes, qmc_quad, romb, simpson, trapezoid
+from ._tanhsinh import nsum, tanhsinh
 
 __all__ = [
     "BDF",
@@ -24,7 +16,6 @@ __all__ = [
     "LSODA",
     "RK23",
     "RK45",
-    "AccuracyWarning",
     "DenseOutput",
     "IntegrationWarning",
     "ODEintWarning",
@@ -32,14 +23,17 @@ __all__ = [
     "OdeSolver",
     "Radau",
     "complex_ode",
+    "cubature",
     "cumulative_simpson",
     "cumulative_trapezoid",
     "dblquad",
     "dop",
     "fixed_quad",
+    "lebedev_rule",
     "lsoda",
     "newton_cotes",
     "nquad",
+    "nsum",
     "ode",
     "odeint",
     "odepack",
@@ -47,12 +41,11 @@ __all__ = [
     "quad",
     "quad_vec",
     "quadpack",
-    "quadrature",
     "romb",
-    "romberg",
     "simpson",
     "solve_bvp",
     "solve_ivp",
+    "tanhsinh",
     "tplquad",
     "trapezoid",
     "vode",
