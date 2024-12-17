b89d67c7c45fd7c7d889be4a8687dad7e7150a26
jorenham <jhammudoglu@gmail.com>
2024-12-17 01:50:57 +0100

    👽️ `linalg`: 1.15.0 support for the (private) `_matfuncs_expm` module (rewritten in C)

--- scipy-stubs/linalg/_matfuncs_expm.pyi
+++ scipy-stubs/linalg/_matfuncs_expm.pyi
@@ -3,9 +3,11 @@ from typing import TypeAlias
 import numpy as np
 import optype.numpy as onp
 
-__all__ = ["pade_UV_calc", "pick_pade_structure"]
-
 _Inexact: TypeAlias = np.float32 | np.float64 | np.complex64 | np.complex128
+
+###
+
+class error(Exception): ...  # undocumented
 
 # `Am` must have a shape like `(5, n, n)`.
 def pick_pade_structure(Am: onp.ArrayND[_Inexact]) -> tuple[int, int]: ...
