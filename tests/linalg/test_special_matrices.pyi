d9096482f28fda9c803699cd0c4ed52032bf6b8f
jorenham <jhammudoglu@gmail.com>
2024-12-17 03:40:28 +0100

    💚 `linalg`: fix failing `numpy<2.1` type-tests

--- tests/linalg/test_special_matrices.pyi
+++ tests/linalg/test_special_matrices.pyi
@@ -6,16 +6,15 @@ from scipy.linalg import kron, toeplitz
 
 arr_f8_1d: onp.Array1D[np.float64]
 arr_f8_2d: onp.Array2D[np.float64]
-arr_f8_3d: onp.Array3D[np.float64]
 arr_f8_nd: onp.ArrayND[np.float64]
 
 ###
 # kron
 # > deprecated
 f_kron_f8_2d = kron(arr_f8_2d, arr_f8_2d)  # pyright: ignore[reportDeprecated]
-f_kron_f8_3d = kron(arr_f8_3d, arr_f8_3d)  # pyright: ignore[reportDeprecated]
+f_kron_f8_nd = kron(arr_f8_nd, arr_f8_nd)  # pyright: ignore[reportDeprecated]
 assert_type(f_kron_f8_2d, onp.Array2D[np.float64])
-assert_type(f_kron_f8_3d, onp.Array[onp.AtLeast2D, np.float64])
+assert_type(f_kron_f8_nd, onp.Array[onp.AtLeast2D, np.float64])
 ###
 
 ###
@@ -32,7 +31,4 @@ assert_type(toeplitz(arr_f8_nd, arr_f8_nd), onp.Array2D[np.float64])
 # > deprecated (raveled input)
 assert_type(toeplitz([[0], [1]]), onp.Array2D[np.int_])  # pyright: ignore[reportDeprecated]
 assert_type(toeplitz([[[0], [1]], [[2], [3]]]), onp.Array2D[np.int_])  # pyright: ignore[reportDeprecated]
-assert_type(toeplitz(arr_f8_2d), onp.Array2D[np.float64])  # pyright: ignore[reportDeprecated]
-assert_type(toeplitz(arr_f8_3d), onp.Array2D[np.float64])  # pyright: ignore[reportDeprecated]
-assert_type(toeplitz(arr_f8_3d, arr_f8_3d), onp.Array2D[np.float64])  # pyright: ignore[reportDeprecated]
 ###
