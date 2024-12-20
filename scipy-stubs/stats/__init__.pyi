cc15c07163ed71f2deb1da0c4a39d515c0f62529
jorenham <jhammudoglu@gmail.com>
2024-12-20 16:33:16 +0100

    🚧 `stats`: temporarily ignore stubtest errors of unfinished stuff

--- scipy-stubs/stats/__init__.pyi
+++ scipy-stubs/stats/__init__.pyi
@@ -75,7 +75,9 @@ from ._multivariate import (
     vonmises_fisher,
     wishart,
 )
-from ._new_distributions import Normal, Uniform
+
+# TODO(jorenham)
+# `from ._new_distributions import Normal, Uniform
 from ._page_trend_test import page_trend_test
 from ._resampling import (
     BootstrapMethod,
@@ -309,9 +311,9 @@ __all__ = [
     "Mixture",
     "MonteCarloMethod",
     "NearConstantInputWarning",
-    "Normal",
+    # "Normal",
     "PermutationMethod",
-    "Uniform",
+    # "Uniform",
     # "abs",
     "alexandergovern",
     "alpha",
---
+++ scipy-stubs/stats/_new_distributions.pyi
@@ -0,0 +1,15 @@
+from ._distribution_infrastructure import ContinuousDistribution
+
+__all__ = ["Normal", "Uniform"]
+
+class Normal(ContinuousDistribution):
+    # TODO(jorenham)
+    ...
+
+class StandardNormal(Normal):
+    # TODO(jorenham)
+    ...
+
+class Uniform(ContinuousDistribution):
+    # TODO(jorenham)
+    ...
