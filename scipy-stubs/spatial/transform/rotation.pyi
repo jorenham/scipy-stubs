e29c62b5d40fd35891c8cfc532bed966bf935bc1
jorenham <jhammudoglu@gmail.com>
2025-01-01 19:24:27 +0100

    🩹 add missing `__all__`

--- scipy-stubs/spatial/transform/rotation.pyi
+++ scipy-stubs/spatial/transform/rotation.pyi
@@ -2,6 +2,8 @@ from typing_extensions import deprecated
 
 from . import _rotation
 
+__all__ = ["Rotation", "Slerp"]
+
 @deprecated("will be removed in SciPy v2.0.0")
 class Rotation(_rotation.Rotation): ...
 
